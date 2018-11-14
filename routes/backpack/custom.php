5caf9230b6f69ed8a600325d85a1dd37ef611408
helesjunior <helesjunior@gmail.com>
2018-11-14 16:26:32 -0200

    alterar pagina inicial

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -7,13 +7,27 @@
 // Routes you generate using Backpack\Generators will be placed here.
 
 Route::group([
-    'prefix'     => config('backpack.base.route_prefix', 'sc'),
+    'prefix' => config('backpack.base.route_prefix', 'sc'),
     'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
-    'namespace'  => 'App\Http\Controllers\Admin',
+    'namespace' => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
-    CRUD::resource('orgaosuperior', 'OrgaosuperiorCrudController');
+    CRUD::resource('orgaosuperior', 'OrgaosuperiorCrudController')->middleware('permission:administracao_orgaosuperior_acesso');
     CRUD::resource('orgao', 'OrgaoCrudController');
     CRUD::resource('unidade', 'UnidadeCrudController');
     CRUD::resource('logactivity', 'LogactivityCrudController');
     CRUD::resource('usuario', 'UsuarioCrudController');
+}); // this should be the absolute last line of this file
+
+Route::group([
+    'prefix' => config('backpack.base.route_prefix', 'sc'),
+    'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
+    'namespace' => 'App\Http\Controllers',
+], function () { // custom admin routes
+
+// if not otherwise configured, setup the dashboard routes
+    if (config('backpack.base.setup_dashboard_routes')) {
+        Route::get('inicio', 'AdminController@index')->name('backpack.inicio');
+        Route::get('/', 'AdminController@redirect')->name('backpack');
+        Route::get('/dashboard', 'AdminController@redirect')->name('backpack');
+    }
 }); // this should be the absolute last line of this file
