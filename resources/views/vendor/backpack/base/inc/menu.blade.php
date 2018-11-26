dfaa529c623335333e0366c05ca8031db93631f3
helesjunior <helesjunior@gmail.com>
2018-11-26 11:42:47 -0200

    backpack new version

---
+++ resources/views/vendor/backpack/base/inc/menu.blade.php
@@ -0,0 +1,55 @@
+<div class="navbar-custom-menu pull-left">
+    <ul class="nav navbar-nav">
+        <!-- =================================================== -->
+        <!-- ========== Top menu items (ordered left) ========== -->
+        <!-- =================================================== -->
+
+        @if (backpack_auth()->check())
+            <!-- Topbar. Contains the left part -->
+            @include('backpack::inc.topbar_left_content')
+        @endif
+
+    <!-- ========== End of top menu left items ========== -->
+    </ul>
+</div>
+
+
+<div class="navbar-custom-menu pull-right">
+    <ul class="nav navbar-nav">
+        <!-- ========================================================= -->
+        <!-- ========= Top menu right items (ordered right) ========== -->
+        <!-- ========================================================= -->
+
+        @if (config('backpack.base.setup_auth_routes'))
+            @if (backpack_auth()->guest())
+                <li>
+                    <a href="{{ url(config('backpack.base.route_prefix', 'admin').'/login') }}">{{ trans('backpack::base.login') }}</a>
+                </li>
+                @if (config('backpack.base.registration_open'))
+                    <li><a href="{{ route('backpack.auth.register') }}">{{ trans('backpack::base.register') }}</a></li>
+                @endif
+            @else
+                <!-- Topbar. Contains the right part -->
+                <li class="dropdown">
+                    {{--<a href="#" data-toggle="control-sidebar" title="Configurações"><i class="fa fa-gears"></i></a>--}}
+                    {{--<li class="dropdown">--}}
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="Configurações"><i
+                                class="fa fa-gears"></i> <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="{{ route('backpack.account.info') }}"><span><i class="fa fa-user-circle-o"></i> {{ trans('backpack::base.my_account') }}</span></a></li>
+                        <li><a href="{{ url('/inicio/mudaug') }}"><span><i class="fa fa-exchange"></i> Mudar UG</a></li>
+                        {{--<li class="divider"></li>--}}
+                        {{--<li><a href="{{ url('/logout') }}" id="logout" title="Sair da Aplicação"--}}
+                        {{--onclick="event.preventDefault();--}}
+                        {{--document.getElementById('logout-form').submit();">--}}
+                        {{--Sair da Aplicação--}}
+                        {{--</a></li>--}}
+                    </ul>
+                </li>
+                @include('backpack::inc.topbar_right_content')
+                <li><a href="{{ route('backpack.auth.logout') }}"><i class="fa fa-btn fa-sign-out"></i> {{ trans('backpack::base.logout') }}</a></li>
+            @endif
+        @endif
+        <!-- ========== End of top menu right items ========== -->
+    </ul>
+</div>
---
+++ routes/backpack/custom.php
@@ -0,0 +1,29 @@
+<?php
+
+// --------------------------
+// Custom Backpack Routes
+// --------------------------
+// This route file is loaded automatically by Backpack\Base.
+// Routes you generate using Backpack\Generators will be placed here.
+
+Route::group([
+    'prefix'     => config('backpack.base.route_prefix', ''),
+    'middleware' => ['web', config('backpack.base.middleware_key', '')],
+    'namespace'  => 'App\Http\Controllers\Admin',
+], function () { // custom admin routes
+}); // this should be the absolute last line of this file
+
+
+Route::group([
+    'prefix' => config('backpack.base.route_prefix', 'sc'),
+    'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
+    'namespace' => 'App\Http\Controllers',
+], function () { // custom admin routes
+
+// if not otherwise configured, setup the dashboard routes
+    if (config('backpack.base.setup_dashboard_routes')) {
+        Route::get('inicio', 'AdminController@index')->name('backpack.inicio');
+        Route::get('/', 'AdminController@redirect')->name('backpack');
+        Route::get('/dashboard', 'AdminController@redirect')->name('backpack');
+    }
+});
