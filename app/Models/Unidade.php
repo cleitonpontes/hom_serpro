739844bba6b8db1ea940de168c961b4993bca85e
70074402153 <heles.junior@planejamento.gov.br>
2018-11-30 19:02:49 -0200

    update

--- app/Models/Unidade.php
+++ app/Models/Unidade.php
@@ -49,12 +49,12 @@ class Unidade extends Model
     public function moreOptions($crud = false)
     {
         $button = '<div class="btn-group">
-                        <button type="button" title="Mais" class="btn btn-xs btn-default dropdown-toggle"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-gear"></i>'.$this->id.'
+                        <button type="button" title="Mais" class="btn btn-xs btn-default dropdown-toggle dropdown-toggle-split"
+                            data-toggle="dropdown"  aria-haspopup="true" aria-expanded="false"><i class="fa fa-gear"></i>'.$this->id.'
                             <span class="caret"></span>
                             <span class="sr-only">Toggle Dropdown</span>
                         </button>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <ul class="dropdown-menu" >
                                 <li><a href="#">Teste 1</a></li>
                                 <li><a href="#">Teste 2</a></li>
                             </ul>
