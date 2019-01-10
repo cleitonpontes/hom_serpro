0b7aed02dda93efd2b56e59642700bb8b2d3ebd9
Heles Resende Silva Junior <heles.junior@AGU.GOV.BR>
2019-01-10 14:53:08 -0200

    docker files

---
+++ down.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo Fechando container da aplicação
+sudo docker-compose down
+
+echo Eliminando imagens
+sudo docker rmi -f 59c542ceb57a
+sudo docker rmi -f 4d73a90f5eef
+
+echo Eliminando bando de dados
+sudo docker volume rm aguconta_conta-postgres-data
