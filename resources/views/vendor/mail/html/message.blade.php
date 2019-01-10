ccf44f879fbdf1de5f1bde978da7ef6162ddf536
Heles Resende Silva Junior <heles.junior@AGU.GOV.BR>
2019-01-10 17:52:02 -0200

    update

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -2,7 +2,7 @@
     {{-- Header --}}
     @slot('header')
         @component('mail::header', ['url' => config('app.url')])
-            <img src="{{ $message->embed(public_path('img/logo.png')) }}" width="130px" alt="Sistema Conta">
+            <img src="http://novo-conta-hom.agu.gov.br/img/logo.png" width="130px" alt="Sistema Conta" data-auto-embed="base64">
         @endcomponent
     @endslot
 
