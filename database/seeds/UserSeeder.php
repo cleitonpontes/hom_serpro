83748e6832e7f797dd06bee316e7d1d2d849fcc9
Heles Resende Silva Junior <heles.junior@AGU.GOV.BR>
2019-02-07 17:56:13 -0200

    update Userseeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,7 +13,7 @@ class UserSeeder extends Seeder
             'name' => 'HELES RESENDE SILVA JÚNIOR',
             'cpf' => '700.744.021-53',
             'email' => 'helesjunior@gmail.com',
-            'ugprimaria' => '1',
+            'ugprimaria' => '8',
             'password' => bcrypt('123456'),
         ]);
         $user->assignRole('Administrador');
@@ -31,7 +31,25 @@ class UserSeeder extends Seeder
             'name' => 'IVALDO DE MESQUITA VERAS',
             'cpf' => '619.798.991-34',
             'email' => 'ivaldo.veras@agu.gov.br',
-            'ugprimaria' => '1',
+            'ugprimaria' => '8',
+            'password' => bcrypt('123456'),
+        ]);
+        $user->assignRole('Administrador');
+
+        $user = \App\Models\BackpackUser::create([
+            'name' => 'Alan Wallace Antunes dos Santos',
+            'cpf' => '034.492.531-58',
+            'email' => 'alan.santos@agu.gov.br',
+            'ugprimaria' => '8',
+            'password' => bcrypt('123456'),
+        ]);
+        $user->assignRole('Administrador');
+
+        $user = \App\Models\BackpackUser::create([
+            'name' => 'Cristiano Roberto Polato Barreira',
+            'cpf' => '001.752.446-69',
+            'email' => 'cristiano.barreira@basis.com.br',
+            'ugprimaria' => '8',
             'password' => bcrypt('123456'),
         ]);
         $user->assignRole('Administrador');
