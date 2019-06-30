e6744d2d0c0e141642a077fed955586ed372b027
heles.junior <heles.junior@agu.gov.br>
2019-06-30 17:18:43 -0300

    ajuste DownloadController para anexos do crud comunica

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -41,6 +41,7 @@ Route::group([
         }
 
         Route::get('/storage/contrato/{pasta}/{file}', 'DownloadsController@contrato');
+        Route::get('/storage/comunica/anexos/{file}', 'DownloadsController@anexoscomunica');
 
 
 
