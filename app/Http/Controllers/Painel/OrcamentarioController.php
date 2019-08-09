eba0ef7a845b4be4ba003b80f6bade70660eec32
heles.junior <heles.junior@agu.gov.br>
2019-08-09 11:44:06 -0300

    update

--- app/Http/Controllers/Painel/OrcamentarioController.php
+++ app/Http/Controllers/Painel/OrcamentarioController.php
@@ -30,20 +30,118 @@ class OrcamentarioController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $this->data['title'] = "Painel Orçamentário";//trans('backpack::base.dashboard'); // set the page title
 
-        $dadosEmpenhosOutrasDespesasCorrentes = $this->retornaDadosEmpenhosOutrasDespesasCorrentes();
+        $dadosEmpenhosOutrasDespesasCorrentes = $this->retornaDadosEmpenhosOutrasDespesasCorrentesArray();
         $graficoEmpenhosOutrasDespesasCorrentes = $this->graficoEmpenhosOutrasDespesasCorrentes($dadosEmpenhosOutrasDespesasCorrentes);
+
+
+        //datatables
+        if ($request->ajax()) {
+            $grid = DataTables::of($dadosEmpenhosOutrasDespesasCorrentes);
+            $grid->editColumn('empenhado', 'R$ {!! number_format(floatval($empenhado), 2, ",", ".") !!}');
+            $grid->editColumn('aliquidar', 'R$ {!! number_format(floatval($aliquidar), 2, ",", ".") !!}');
+            $grid->editColumn('liquidado', 'R$ {!! number_format(floatval($liquidado), 2, ",", ".") !!}');
+            $grid->editColumn('pago', 'R$ {!! number_format(floatval($pago), 2, ",", ".") !!}');
+            $grid->with([
+                'empenhado' => 0.00,
+                'aliquidar' => 0.00,
+                'liquidado' => 0.00,
+                'pago' => 0.00,
+            ]);
+            return $grid->make(true);
+        }
+
+        $html = $this->retornaGrid();
 
 
         return view('backpack::mod.paineis.painelorcamentario',
             [
                 'data' => $this->data,
                 'graficoEmpenhosOutrasDespesasCorrentes' => $graficoEmpenhosOutrasDespesasCorrentes,
-                'dadosEmpenhosOutrasDespesasCorrentes' => $dadosEmpenhosOutrasDespesasCorrentes
+                'dadosEmpenhosOutrasDespesasCorrentes' => $dadosEmpenhosOutrasDespesasCorrentes,
+                'html' => $html
             ]);
+    }
+
+    private function retornaGrid()
+    {
+        $html = $this->htmlBuilder;
+
+        $html->addColumn([
+            'data' => 'nome',
+            'name' => 'nome',
+            'title' => 'Unidade Gestora'
+        ]);
+        $html->addColumn([
+            'data' => 'empenhado',
+            'name' => 'empenhado',
+            'title' => 'Empenhado'
+        ]);
+        $html->addColumn([
+            'data' => 'aliquidar',
+            'name' => 'aliquidar',
+            'title' => 'A Liquidar'
+        ]);
+        $html->addColumn([
+            'data' => 'liquidado',
+            'name' => 'liquidado',
+            'title' => 'Liquidado'
+        ]);
+        $html->addColumn([
+            'data' => 'pago',
+            'name' => 'pago',
+            'title' => 'Pago'
+        ]);
+
+        $html->parameters([
+
+            'processing' => true,
+            'serverSide' => true,
+            'responsive' => true,
+            'info' => true,
+            'autoWidth' => false,
+            'bAutoWidth' => false,
+            'paging' => true,
+            'lengthChange' => true,
+            'language' => [
+                'url' => asset('/json/pt_br.json')
+            ],
+//            'dom'          => 'Bfrtip',
+//            'buttons'      => ['export', 'print'],
+
+        ]);
+
+        return $html;
+    }
+
+
+
+    private function retornaDadosEmpenhosOutrasDespesasCorrentesArray()
+    {
+
+        $valores_empenhos = Empenho::whereHas('unidade', function ($q) {
+            $q->where('situacao', '=', true);
+        });
+        $valores_empenhos->whereHas('naturezadespesa', function ($q) {
+            $q->where('codigo', 'LIKE', '33%');
+        });
+        $valores_empenhos->leftjoin('unidades', 'empenhos.unidade_id', '=', 'unidades.id');
+        $valores_empenhos->orderBy('nome');
+        $valores_empenhos->groupBy('unidades.codigo');
+        $valores_empenhos->groupBy('unidades.nomeresumido');
+        $valores_empenhos->select([
+            DB::raw("unidades.codigo ||' - '||unidades.nomeresumido as nome"),
+            DB::raw('sum(empenhos.empenhado) as empenhado'),
+            DB::raw("sum(empenhos.aliquidar) as aliquidar"),
+            DB::raw("sum(empenhos.liquidado) as liquidado"),
+            DB::raw("sum(empenhos.pago) as pago")
+        ]);
+
+        return $valores_empenhos->get()->toArray();
+
     }
 
     private function retornaDadosEmpenhosOutrasDespesasCorrentes()
@@ -67,7 +165,7 @@ class OrcamentarioController extends Controller
             DB::raw("sum(empenhos.pago) as pago")
         ]);
 
-        return $valores_empenhos->get()->toArray();
+        return $valores_empenhos->get();
 
     }
 
--- resources/views/vendor/backpack/base/mod/paineis/painelorcamentario.blade.php
+++ resources/views/vendor/backpack/base/mod/paineis/painelorcamentario.blade.php
@@ -33,48 +33,9 @@
                         <div class="box-body">
                             {!! $graficoEmpenhosOutrasDespesasCorrentes->render() !!}
                             <br><br>
-                            <table class="table table-striped table-hover table-bordered table-responsive">
-                                <thead>
-                                <tr>
-                                    <td align="center"><b>Unidade Gestora</b></td>
-                                    <td align="center"><b>Empenhado</b></td>
-                                    <td align="center"><b>A Liquidar</b></td>
-                                    <td align="center"><b>Liquidado</b></td>
-                                    <td align="center"><b>Pago</b></td>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @php
-                                    $tfootdadosEmpenhosOutrasDespesasCorrentes['empenhado'] = 0;
-                                    $tfootdadosEmpenhosOutrasDespesasCorrentes['aliquidar'] = 0;
-                                    $tfootdadosEmpenhosOutrasDespesasCorrentes['liquidado'] = 0;
-                                    $tfootdadosEmpenhosOutrasDespesasCorrentes['pago'] = 0;
-                                @endphp
-                                @foreach($dadosEmpenhosOutrasDespesasCorrentes as $dadoEmpenhoOutrasDespesasCorrentes)
-                                    <tr>
-                                        <td>{{$dadoEmpenhoOutrasDespesasCorrentes['nome'] }}</td>
-                                        <td align="right">R$ {{number_format($dadoEmpenhoOutrasDespesasCorrentes['empenhado'],2,',','.')}}</td>
-                                        <td align="right">R$ {{number_format($dadoEmpenhoOutrasDespesasCorrentes['aliquidar'],2,',','.')}}</td>
-                                        <td align="right">R$ {{number_format($dadoEmpenhoOutrasDespesasCorrentes['liquidado'],2,',','.')}}</td>
-                                        <td align="right">R$ {{number_format($dadoEmpenhoOutrasDespesasCorrentes['pago'],2,',','.')}}</td>
-                                    </tr>
-                                @php
-                                    $tfootdadosEmpenhosOutrasDespesasCorrentes['empenhado'] += $dadoEmpenhoOutrasDespesasCorrentes['empenhado'];
-                                    $tfootdadosEmpenhosOutrasDespesasCorrentes['aliquidar'] += $dadoEmpenhoOutrasDespesasCorrentes['aliquidar'];
-                                    $tfootdadosEmpenhosOutrasDespesasCorrentes['liquidado'] += $dadoEmpenhoOutrasDespesasCorrentes['liquidado'];
-                                    $tfootdadosEmpenhosOutrasDespesasCorrentes['pago'] += $dadoEmpenhoOutrasDespesasCorrentes['pago'];
-                                @endphp
-                                @endforeach
-                                <tfoot>
-                                <tr>
-                                    <td align="center"><b>Total</b></td>
-                                    <td align="right"><b>R$ {{number_format($tfootdadosEmpenhosOutrasDespesasCorrentes['empenhado'],2,',','.')}}</b></td>
-                                    <td align="right"><b>R$ {{number_format($tfootdadosEmpenhosOutrasDespesasCorrentes['aliquidar'],2,',','.')}}</b></td>
-                                    <td align="right"><b>R$ {{number_format($tfootdadosEmpenhosOutrasDespesasCorrentes['liquidado'],2,',','.')}}</b></td>
-                                    <td align="right"><b>R$ {{number_format($tfootdadosEmpenhosOutrasDespesasCorrentes['pago'],2,',','.')}}</b></td>
-                                </tr>
-                                </tfoot>
-                            </table>
+                            <div class="col-sm-12">
+                                {!! $html->table() !!}
+                            </div>
                         </div>
                         <!-- /.box-body -->
                     </div>
@@ -98,6 +59,9 @@
                         </div>
                         <!-- /.box-header -->
                         <div class="box-body no-padding">
+                            <div class="col-sm-12">
+
+                            </div>
                         </div>
                         <!-- /.box-body -->
                     </div>
@@ -105,4 +69,13 @@
             </div>
         </div>
     </div>
+
 @endsection
+@push('after_scripts')
+    {{--<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.0.3/css/buttons.dataTables.min.css">--}}
+    {{--<script src="https://cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js"></script>--}}
+    {{--<script src="/vendor/datatables/buttons.server-side.js"></script>--}}
+    {!! $html->scripts() !!}
+
+@endpush
+
