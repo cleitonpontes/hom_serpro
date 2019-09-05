9c04f9d4b0eb41b4b612a97571577d6e5c29ba02
heles.junior <heles.junior@agu.gov.br>
2019-09-05 09:37:36 -0300

    bug atualização saldo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new MigracaoempenhoJob)->dailyAt('08:45');
-        $schedule->job(new MigracaoempenhoJob)->everyMinute();
+//        $schedule->job(new MigracaoempenhoJob)->everyMinute();
         $schedule->call('App\Http\Controllers\Execfin\EmpenhoCrudController@atualizaSaldosEmpenhos')->dailyAt('08:50');
         $schedule->job(new AlertaContratoJob)->dailyAt('08:00');
     }
