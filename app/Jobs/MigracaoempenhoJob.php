a2e6d7e952df8dc8636335eeaa66fdb6b7777e0d
heles.junior <heles.junior@agu.gov.br>
2019-09-05 09:53:46 -0300

    bug atualização saldo

--- app/Jobs/MigracaoempenhoJob.php
+++ app/Jobs/MigracaoempenhoJob.php
@@ -105,8 +105,7 @@ class MigracaoempenhoJob implements ShouldQueue
 
     public function atualizaSaldosEmpenhos()
     {
-        $empenhos = Empenho::where('id', $unidade_id)
-            ->get();
+        $empenhos = Empenho::all();
 
         $amb = 'PROD';
         $meses = array('', 'JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ');
