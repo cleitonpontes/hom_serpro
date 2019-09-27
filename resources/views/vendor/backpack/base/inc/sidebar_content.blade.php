9e6335fe25785001d4cd3fea15d99519b6e3209f
helesjunior <helesjunior@gmail.com>
2019-09-27 08:05:27 -0300

    relatorio contratos

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -4,7 +4,7 @@
     <a href="#"><i class="fa fa-bar-chart"></i> <span>Painéis</span> <i
             class="fa fa-angle-left pull-right"></i></a>
     <ul class="treeview-menu">
-        <li><a href="https://app.powerbi.com/view?r=eyJrIjoiYzQxMmQwZjgtMGJkOC00OGY4LTk2MmItMWVjMGU5NmNiMTBhIiwidCI6IjRkNzlkMzdhLTFlNGUtNGEzOS05ZmRlLWYxNjMxY2I2MDdkNCJ9" target="_blank"><i class="fa fa-money"></i>
+        <li><a href="https://app.powerbi.com/view?r=eyJrIjoiYzQxMmQwZjgtMGJkOC00OGY4LTk2MmItMWVjMGU5NmNiMTBhIiwidCI6IjRkNzlkMzdhLTFlNGUtNGEzOS05ZmRlLWYxNjMxY2I2MDdkNCJ9" target="_blank"><i class="fa fa-dashboard"></i>
                 <span>Contratos</span></a></li>
         <li class="treeview">
             <a href="#"><i class='fa fa-edit'></i> <span>Orçamento e Finanças</span> <i
