31692aa5cbdf5ea8245ffe5214a16865a04b0c77
helesjunior <helesjunior@gmail.com>
2019-09-27 08:02:45 -0300

    relatorio contratos

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -4,6 +4,8 @@
     <a href="#"><i class="fa fa-bar-chart"></i> <span>Painéis</span> <i
             class="fa fa-angle-left pull-right"></i></a>
     <ul class="treeview-menu">
+        <li><a href="https://app.powerbi.com/view?r=eyJrIjoiYzQxMmQwZjgtMGJkOC00OGY4LTk2MmItMWVjMGU5NmNiMTBhIiwidCI6IjRkNzlkMzdhLTFlNGUtNGEzOS05ZmRlLWYxNjMxY2I2MDdkNCJ9" target="_blank"><i class="fa fa-money"></i>
+                <span>Contratos</span></a></li>
         <li class="treeview">
             <a href="#"><i class='fa fa-edit'></i> <span>Orçamento e Finanças</span> <i
                     class="fa fa-angle-left pull-right"></i></a>
@@ -12,10 +14,10 @@
                 {{--<span>Financeiro</span></a></li>--}}
                 <li><a href="{{ backpack_url('/painel/orcamentario') }}"><i class="fa fa-money"></i>
                         <span>Orçamentário</span></a></li>
-                <li><a href="https://app.powerbi.com/view?r=eyJrIjoiYzQxMmQwZjgtMGJkOC00OGY4LTk2MmItMWVjMGU5NmNiMTBhIiwidCI6IjRkNzlkMzdhLTFlNGUtNGEzOS05ZmRlLWYxNjMxY2I2MDdkNCJ9" target="_blank"><i class="fa fa-money"></i>
-                        <span>Contratos</span></a></li>
+
             </ul>
         </li>
+
     </ul>
 </li>
 <li class="treeview">
