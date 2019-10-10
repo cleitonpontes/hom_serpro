134f7220210dbc402fdc7e08e95f713e17f97a43
helesjunior <helesjunior@gmail.com>
2019-10-10 09:52:54 -0300

    bug alerta extrato mensal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,8 +33,8 @@ class Kernel extends ConsoleKernel
 
         $schedule->call('App\Http\Controllers\Execfin\EmpenhoCrudController@executaAtualizaSaldosEmpenhos')->dailyAt('08:50');
 
-//        $schedule->job(new AlertaContratoJob)->dailyAt('08:00');
-        $schedule->job(new AlertaContratoJob)->everyFiveMinutes();
+        $schedule->job(new AlertaContratoJob)->dailyAt('08:00');
+//        $schedule->job(new AlertaContratoJob)->everyFiveMinutes();
     }
 
     /**
