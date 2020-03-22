0732c541fed861fc35602976f9044fc01c502f8c
heles.junior <heles.junior@agu.gov.br>
2020-03-21 21:45:52 -0300

    implementacao da funcionalidade de subrogacao

--- app/Observers/SubrogacaoObserver.php
+++ app/Observers/SubrogacaoObserver.php
@@ -11,14 +11,12 @@ class SubrogacaoObserver
     public function created(Subrogacao $subrogacao)
     {
         $this->atualizaContrato($subrogacao->unidadedestino_id, $subrogacao->contrato_id);
-        $this->atualizaHistorico($subrogacao->unidadedestino_id, $subrogacao->contrato_id);
     }
 
 
     public function deleted(Subrogacao $subrogacao)
     {
         $this->atualizaContrato($subrogacao->unidadeorigem_id, $subrogacao->contrato_id);
-        $this->atualizaHistorico($subrogacao->unidadeorigem_id, $subrogacao->contrato_id);
     }
 
     private function atualizaContrato($unidadedestino,$contrato_id)
@@ -26,17 +24,13 @@ class SubrogacaoObserver
         $contrato = Contrato::find($contrato_id);
         $contrato->unidade_id = $unidadedestino;
         $contrato->save();
-    }
 
-    private function atualizaHistorico($unidadedestino,$contrato_id)
-    {
-
-        $historicos = Contratohistorico::where('contrato_id',$contrato_id)->get();
+        $historicos = $contrato->historico();
         foreach ($historicos as $historico){
             $historico->unidade_id = $unidadedestino;
             $historico->save();
         }
-
     }
+
 
 }
