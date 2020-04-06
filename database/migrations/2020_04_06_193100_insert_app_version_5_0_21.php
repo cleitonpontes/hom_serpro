6042920cb1a0626a0afa97dbce9875af7508896f
Heles Junior <helesjunior@gmail.com>
2020-04-06 16:32:51 -0300

    Adequação Integração do Daas Serpro

---
+++ database/migrations/2020_04_06_193100_insert_app_version_5_0_21.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class InsertAppVersion5021 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \App\Models\AppVersion::create([
+            'major' => 5,
+            'minor' => 0,
+            'patch' => 21
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        \App\Models\AppVersion::where([
+            'major' => 5,
+            'minor' => 0,
+            'patch' => 21
+        ])->forceDelete();
+    }
+}
