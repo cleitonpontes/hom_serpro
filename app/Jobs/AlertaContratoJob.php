f1b42299ece316f8f17f2a07041ec72caf375c3f
Anderson Sathler <asathler@gmail.com>
2020-06-08 20:17:34 -0300

    #24 Email diário único

--- app/Jobs/AlertaContratoJob.php
+++ app/Jobs/AlertaContratoJob.php
@@ -5,12 +5,14 @@ namespace App\Jobs;
 use App\Models\BackpackUser;
 use App\Models\Contrato;
 use App\Models\Unidade;
+use App\Notifications\RotinaAlertaContratoDiarioNotification;
 use App\Notifications\RotinaAlertaContratoNotification;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Notification;
 use function foo\func;
 
 class AlertaContratoJob implements ShouldQueue
@@ -43,24 +45,16 @@ class AlertaContratoJob implements ShouldQueue
         $unidades = $this->retornaUnidadesQueEnviamEmail();
 
         foreach($unidades as $unidade) {
-            // Retorna prazos para envio do email diário
-            $prazos = explode(';', $unidade->configuracao->email_diario_periodicidade);
-
+            $hoje = date('Y-m-d');
             $vencimentos = [];
 
-            $hoje = date('Y-m-d');
-            //
-            // Testes...
-            //
-            $hoje = '2020-06-04'; // 7 contratos [5275, 5347, 5469, 5487, 5691, 5791, 5794]
-            // $hoje = '2020-06-10'; // 4 contratos [5587, 5630, 5713, 5820]
+            $prazos = $this->retornaPrazosTratados($unidade->configuracao->email_diario_periodicidade);
 
             foreach($prazos as $prazo) {
                 $venc = date('Y-m-d', strtotime('+' . $prazo . ' days', strtotime($hoje)));
                 $vencimentos[$prazo] = $venc;
             }
 
-            // Retorna contratos da unidade com final da vigência = $vencimentos
             $contratos = $this->retornaContratosDaUnidade($unidade, $vencimentos);
 
             foreach($contratos as $contrato) {
@@ -68,23 +62,11 @@ class AlertaContratoJob implements ShouldQueue
                 $dtFim = new \DateTime($contrato->vigencia_fim);
                 $qtdeDias = $dtFim->diff($dtAgora)->days;
 
-                foreach ($contrato->responsaveis as $responsavel) {
-                    $situacaoUsuario = $responsavel->situacao;
+                $usuarios = $this->retornaUsuariosDaUnidadeEDoContrato($unidade, $contrato);
+                $primeiroUsuario = array_shift($usuarios);
 
-                    if ($situacaoUsuario == true) {
-                        $nomeUsuario = $responsavel->user->name;
-
-                        // Monta dados para envio do email
-                        $dadosEmail = $this->retornaDadosParaEmail($unidade, $qtdeDias, $nomeUsuario);
-
-                        $user = $responsavel->user;
-                        $user->notify(new RotinaAlertaContratoNotification($user, $dadosEmail, $contrato));
-                    }
-                }
-
-                // Se tenha user1, user2, user3, user4...
-                // Enviar um email cc para cada (ao invés de uma cópia por responsável
-                // Nesse caso, deve-se remover a parte de Usuários em $this->retornaDadosParaEmail
+                $dadosEmail = $this->retornaDadosParaEmail($unidade, $qtdeDias, $usuarios);
+                $primeiroUsuario->notify(new RotinaAlertaContratoDiarioNotification($dadosEmail, $contrato));
             }
         }
     }
@@ -174,14 +156,57 @@ class AlertaContratoJob implements ShouldQueue
         return $dados->get();
     }
 
-    private function retornaDadosParaEmail($unidade, $qtdeDias = 0, $nomeUsuario = '')
+    private function retornaUsuariosDaUnidadeEDoContrato($unidade, $contrato)
     {
-        // Preparações
+        $usuarios = [];
+
+        // Retorna usuários responsáveis do contrato...
+        $responsaveis = $contrato->responsaveis;
+
+        foreach ($responsaveis as $responsavel) {
+            if ($responsavel->situacao == true) {
+                // $usuarios[] = $responsavel->user->toArray();
+                $usuarios[] = $responsavel->user;
+            }
+        }
+
+        // ...e os usuários (de 1 a 4) da configuração da unidade
+        $config = $unidade->configuracao;
+
+        if ($config->user1) {
+            // $usuarios[] = $config->user1->toArray();
+            $usuarios[] = $config->user1;
+        }
+
+        if ($config->user2) {
+            // $usuarios[] = $config->user2->toArray();
+            $usuarios[] = $config->user2;
+        }
+
+        if ($config->user3) {
+            // $usuarios[] = $config->user3->toArray();
+            $usuarios[] = $config->user3;
+        }
+
+        if ($config->user4) {
+            // $usuarios[] = $config->user4->toArray();
+            $usuarios[] = $config->user4;
+        }
+
+        // Elimina usuários 'repetidos'!
+        $usuariosUnicos = array_unique($usuarios);
+
+        return $usuariosUnicos;
+    }
+
+    private function retornaDadosParaEmail($unidade, $qtdeDias = 0, $usuarios)
+    {
+        // Prepara dados para envio do email
         $rotina = 'Contratos à vencer em: ' . $qtdeDias . ' Dias!';
 
         $textoDiario = $unidade->configuracao->email_diario_texto;
         $textoConvertido = mb_convert_encoding($textoDiario, 'UTF-8', 'UTF-8');
-        $texto = str_replace('!!nomeresponsavel!!', $nomeUsuario, $textoConvertido);
+        $texto = str_replace('!!nomeresponsavel!!', 'Responsável', $textoConvertido);
 
         $telefones = $unidade->configuracao->telefone1;
         $telefones .= ($unidade->configuracao->telefone2) ? ' / ' . $unidade->configuracao->telefone2 : '';
@@ -191,27 +216,27 @@ class AlertaContratoJob implements ShouldQueue
         $dadosEmail['telefones'] = $telefones;
         $dadosEmail['texto'] = $texto;
 
-        // Usuários em cópia
-        $user1 = $unidade->configuracao->user1;
-        $user2 = $unidade->configuracao->user2;
-        $user3 = $unidade->configuracao->user3;
-        $user4 = $unidade->configuracao->user4;
-
-        $dadosEmail['copiados']['user1'] = $user1;
-
-        if ($user2) {
-            $dadosEmail['copiados']['user2'] = $user2;
-        }
-
-        if ($user3) {
-            $dadosEmail['copiados']['user3'] = $user3;
-        }
-
-        if ($user4) {
-            $dadosEmail['copiados']['user4'] = $user4;
-        }
+        // Usuários destinatários
+        $dadosEmail['usuarios'] = $usuarios;
 
         return $dadosEmail;
+    }
+
+    private function retornaPrazosTratados($periodicidades)
+    {
+        $separadoresInvalidos = [' ', '-', '_', '.', ',', '/'];
+        $separadorValido = ';';
+
+        // Retorna prazos para envio do email diário
+        $periodosTratados = (!is_null($periodicidades) && $periodicidades != '') ? $periodicidades : '';
+
+        foreach ($separadoresInvalidos as $caracter) {
+            $periodosTratados = str_replace($caracter, $separadorValido, $periodosTratados);
+        }
+
+        $prazos = explode($separadorValido, $periodosTratados);
+
+        return $prazos;
     }
 
 }
