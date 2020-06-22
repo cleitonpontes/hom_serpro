e529f37c52a82d52c7abf480634ebe5e90ebd751
Franklin <franklin.linux@gmail.com>
2020-06-22 16:18:47 -0300

    Ajustes: - validações no ContratosfpadraoRequest.php - criadas as migrations de permissão de acesso a controller - habilitados os botões de exportação - alterado o título da CRUD  para  Padrões de Execução SIAFI - Contrato - criados os Joins e o SearchLogic para os campos de busca - criado o observer ContratosfpadraoObserver - registrado no AppServiceProvider.php o observer ContratosfpadraoObserver

---
+++ app/Observers/ContratosfpadraoObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\ModelsContratosfpadrao;
+
+class ContratosfpadraoObserver
+{
+    /**
+     * Handle the models contratosfpadrao "created" event.
+     *
+     * @param  \App\ModelsContratosfpadrao  $modelsContratosfpadrao
+     * @return void
+     */
+    public function created(ModelsContratosfpadrao $modelsContratosfpadrao)
+    {
+        //
+    }
+
+    /**
+     * Handle the models contratosfpadrao "updated" event.
+     *
+     * @param  \App\ModelsContratosfpadrao  $modelsContratosfpadrao
+     * @return void
+     */
+    public function updated(ModelsContratosfpadrao $modelsContratosfpadrao)
+    {
+        //
+    }
+
+    /**
+     * Handle the models contratosfpadrao "deleted" event.
+     *
+     * @param  \App\ModelsContratosfpadrao  $modelsContratosfpadrao
+     * @return void
+     */
+    public function deleted(ModelsContratosfpadrao $modelsContratosfpadrao)
+    {
+        //
+    }
+
+    /**
+     * Handle the models contratosfpadrao "restored" event.
+     *
+     * @param  \App\ModelsContratosfpadrao  $modelsContratosfpadrao
+     * @return void
+     */
+    public function restored(ModelsContratosfpadrao $modelsContratosfpadrao)
+    {
+        //
+    }
+
+    /**
+     * Handle the models contratosfpadrao "force deleted" event.
+     *
+     * @param  \App\ModelsContratosfpadrao  $modelsContratosfpadrao
+     * @return void
+     */
+    public function forceDeleted(ModelsContratosfpadrao $modelsContratosfpadrao)
+    {
+        //
+    }
+}
