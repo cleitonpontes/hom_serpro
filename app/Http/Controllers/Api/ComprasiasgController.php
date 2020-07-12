be1cb5424f7db95d68db01672cd9de1ad8d5218e
Heles junior <helesjunior@gmail.com>
2020-07-12 17:59:33 -0300

    Formulário ContratoSIASG

---
+++ app/Http/Controllers/Api/ComprasiasgController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Unidade;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UnidadeController extends Controller
+{
+    public function index(Request $request)
+    {
+        $search_term = $request->input('q');
+        $page = $request->input('page');
+
+        if ($search_term)
+        {
+            $results = Unidade::where('codigo', 'LIKE', '%'.strtoupper($search_term).'%')
+                ->orWhere('nome', 'LIKE', '%'.strtoupper($search_term).'%')
+                ->orWhere('nomeresumido', 'LIKE', '%'.strtoupper($search_term).'%')
+                ->paginate(10);
+        }
+        else
+        {
+            $results = Unidade::paginate(10);
+        }
+
+        return $results;
+    }
+
+    public function show($id)
+    {
+        return Unidade::find($id);
+    }
+}
