0999af39f1c0d2796d07e2464aab33ba0749370b
Alexssander Saraiva <alexssander.martins@tse.jus.br>
2020-07-16 15:30:14 -0300

    Refatorando a Seeder 'UpdateNumeroContratoeHistoricoSeeder.php', pois o Job 'AtualizaMascaraJob' foi criado e agora qualquer necessidade de alteração de mascara via 'LPAD' pode ser utilizado pelo JOB

--- database/seeds/UpdateNumeroContratoeHistoricoSeeder.php
+++ database/seeds/UpdateNumeroContratoeHistoricoSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\AlteraMascaraJob;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 // composer require laracasts/testdummy
@@ -7,47 +8,9 @@ use Laracasts\TestDummy\Factory as TestDummy;
 
 class UpdateNumeroContratoeHistoricoSeeder extends Seeder
 {
-    private $tabela;
-    private $campo;
-    private $tamanhoCampo;
-    private $preenchimento;
-    private $relacionamentos;
-
-    public function __construct()
-    {   
-        $this->tabela = "contratos";
-        $this->campo = "numero";
-        $this->tamanhoCampo = 10;
-        $this->preenchimento = "0";
-        //Criado em forma de array, para que possiveis futuras atualizações sejam realizadas na mesma transação
-        $this->relacionamentos = ['contratohistorico'];
-    }
-
     public function run()
     {
-        DB::transaction(function () {
-            $this->completarMascara($this->tabela,$this->campo,$this->tamanhoCampo, $this->preenchimento);
-            foreach ($this->relacionamentos as $key => $tabelaRelacionada) {
-                $this->completarMascara($tabelaRelacionada,$this->campo,$this->tamanhoCampo, $this->preenchimento);
-            }
-        });
+        $arrumaCampo = new AlteraMascaraJob("contratos","numero",10,"0",['contratohistorico']);
+        $arrumaCampo->handle();
     }
-
-    private function completarMascara($tabela, $campo, $tamanhoCampo, $preenchimento){
-        //Atualizar a tabela X, onde o campo P seja menor que T. 
-        //Formate o campo utilizando a função LPAD, preenchendo-o com Z até o tamanho T
-        $contAtualizados = DB::table($tabela)
-        ->whereRaw("LENGTH(".$campo.") < ?", $tamanhoCampo)
-        ->update(
-            [$campo => DB::raw("lpad(".$campo.",".$tamanhoCampo.",'".$preenchimento."')")]
-        );
-        var_dump($contAtualizados);
-        //$this->contaErrados($tabela,$campo,$tamanhoCampo);
-    }
-/*
-    private function contaErrados($tabela, $campo, $tamanhoCampo){
-        $errados = DB::table($tabela)->whereRaw("LENGTH(".$campo.") > ?", $tamanhoCampo)->get();
-        var_dump($errados->count());
-    }
-    */
 }
