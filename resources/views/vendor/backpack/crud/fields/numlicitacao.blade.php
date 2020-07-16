f3470b977d6beb21a65fff3787974401ccfec2c7
Alexssander Saraiva <alexssander.martins@tse.jus.br>
2020-07-16 12:30:46 -0300

    Mascara do campo 'Número Licitação' (numlicitacao) alterado de '9999/9999' para '99999/9999'

--- resources/views/vendor/backpack/crud/fields/numlicitacao.blade.php
+++ resources/views/vendor/backpack/crud/fields/numlicitacao.blade.php
@@ -23,7 +23,7 @@
 
 @push('crud_fields_scripts')
     <script type="text/javascript">
-        $('#{{ $field['name'] }}').mask('9999/9999');
+        $('#{{ $field['name'] }}').mask('99999/9999');
     </script>
 @endpush
 
