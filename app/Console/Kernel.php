727eef9d4f848c564010edf05d0d4762814e7923
Heles junior <helesjunior@gmail.com>
2020-07-21 15:18:28 -0300

    Atualizaçao kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -44,10 +44,10 @@ class Kernel extends ConsoleKernel
             ->timezone('America/Sao_Paulo')
             ->at('08:50');
 
-//        $schedule->call('App\Http\Controllers\Gescon\ContratosfpadraoCrudController@executaJobAtualizacaoSfPadrao')
-//            ->weekdays()
-//            ->timezone('America/Sao_Paulo')
-//            ->everyMinute();
+        $schedule->call('App\Http\Controllers\Gescon\ContratosfpadraoCrudController@executaJobAtualizacaoSfPadrao')
+            ->weekdays()
+            ->timezone('America/Sao_Paulo')
+            ->everyMinute();
 
         $schedule->call('App\Jobs\LimpaActivityLogJob@handle')
             ->weekdays()
