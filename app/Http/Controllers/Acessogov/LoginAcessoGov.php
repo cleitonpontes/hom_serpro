e96d9dedb4cca29aa215defad4a1043088ebc67c
franklin.silva <franklin.linux@gmail.com>
2020-08-11 15:50:13 -0300

    Debug redirect_uri

--- app/Http/Controllers/Acessogov/LoginAcessoGov.php
+++ app/Http/Controllers/Acessogov/LoginAcessoGov.php
@@ -64,7 +64,7 @@ class LoginAcessoGov extends Controller
             $campos = array(
                 'grant_type' => 'authorization_code',
                 'code' => $request->get('code'),
-                'redirect_uri' => urlencode('https://sc-treino.agu.gov.br/acessogov/login')
+                'redirect_uri' => urlencode('https://sc-treino.agu.gov.br/login')
             );
 
             foreach($campos as $key=>$value) {
