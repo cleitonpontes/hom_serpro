e18d18ae67cfe515553edecc2763d777c4a05fa2
franklin.silva <Fr4zkl1z>
2020-08-11 10:44:10 -0300

    debug code

--- app/Http/Controllers/Acessogov/LoginAcessoGov.php
+++ app/Http/Controllers/Acessogov/LoginAcessoGov.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Redirect;
 use App\Http\Requests\Request;
 
+
 class LoginAcessoGov extends Controller
 {
     private $host_acessogov;
