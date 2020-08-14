51f7226e3dfa971a5b3222df9cbe1722a9da6600
franklin.silva <franklin.linux@gmail.com>
2020-08-14 14:24:29 -0300

    ajuste na migration

--- database/migrations/2020_08_14_114559_add_padrao_processo_mascara_table_unidadeconfiguracao.php
+++ database/migrations/2020_08_14_114559_add_padrao_processo_mascara_table_unidadeconfiguracao.php
@@ -14,7 +14,7 @@ class AddPadraoProcessoMascaraTableUnidadeconfiguracao extends Migration
     public function up()
     {
         Schema::table('unidadeconfiguracao', function (Blueprint $table) {
-            $table->string('padrao_processo_mascara')->after('unidade_id');
+            $table->string('padrao_processo_mascara')->nullable()->after('unidade_id');
         });
     }
 
