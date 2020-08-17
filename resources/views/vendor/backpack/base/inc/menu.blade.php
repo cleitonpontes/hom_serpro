a5df06cd47d0f2f5d2c3160f5855ae6c4affa8a5
franklin.silva <Fr4zkl1z>
2020-08-17 10:21:22 -0300

    Validações para usuário sem UG

--- resources/views/vendor/backpack/base/inc/menu.blade.php
+++ resources/views/vendor/backpack/base/inc/menu.blade.php
@@ -30,7 +30,9 @@
                 @endif
             @else
             <!-- Topbar. Contains the right part -->
-                <li><a href="{{ url('/mudar-ug') }}"><span><i class="fa fa-exchange"></i> Mudar UG/UASG</a></li>
+                @if((!is_null(session('user_ug'))))
+                    <li><a href="{{ url('/mudar-ug') }}"><span><i class="fa fa-exchange"></i> Mudar UG/UASG</a></li>
+                @endif
                 <li><a href="{{ route('inicio.meusdados') }}"><span><i class="fa fa-user-circle-o"></i> {{ trans('backpack::base.my_account') }}</span></a>
                 </li>
                 {{--<li class="dropdown">--}}
