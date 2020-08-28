3f3b8c934ac84d581964772d55e3b3b99867cd3a
Marcio L Vasconcelos Donato <mvascs@gmail.com>
2020-08-28 10:02:13 -0300

    retirado o link da função

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -446,7 +446,6 @@ class AdminController extends Controller
 
     public function buscaDadosUrlMigracao($url)
     {
-        echo '<br>'.$url;
         return json_decode(file_get_contents($url), true);
     }
 
