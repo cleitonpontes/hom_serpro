2751ee3c827a0d293068a173f759bbf529b0defd
Anderson Sathler <asathler@gmail.com>
2020-09-24 17:57:13 -0300

    Jobs no Kernel em progresso...

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,56 +25,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call('App\Http\Controllers\Gescon\ContratosfpadraoCrudController@executaJobAtualizacaoSfPadrao')
-            ->weekdays()
-            ->timezone('America/Sao_Paulo')
-            ->everyMinute();
-
-//        $schedule->call('App\Http\Controllers\Gescon\Siasg\SiasgcontratoCrudController@executaJobAtualizacaoSiasgContratos')
-//            ->weekdays()
-//            ->timezone('America/Sao_Paulo')
-//            ->everyMinute();
-
-//        $schedule->call('App\Http\Controllers\Gescon\Siasg\SiasgcompraCrudController@executaJobAtualizacaoSiasgCompras')
-//            ->weekdays()
-//            ->timezone('America/Sao_Paulo')
-//            ->everyMinute();
-
-        $schedule->call('App\Http\Controllers\Admin\AlertaContratoController@enviaEmails')
-            ->timezone('America/Sao_Paulo')
-            ->dailyAt('08:00');
-
-        $schedule->call('App\Http\Controllers\Execfin\EmpenhoCrudController@executaAtualizacaoNd')
-            ->weekdays()
-            ->timezone('America/Sao_Paulo')
-            ->at('08:30');
-
-        $schedule->call('App\Http\Controllers\Execfin\EmpenhoCrudController@executaMigracaoEmpenho')
-            ->weekdays()
-            ->timezone('America/Sao_Paulo')
-            ->at('08:40');
-
-        $schedule->call('App\Http\Controllers\Execfin\EmpenhoCrudController@executaAtualizaSaldosEmpenhos')
-            ->weekdays()
-            ->timezone('America/Sao_Paulo')
-            ->at('08:50');
-
-        $schedule->call('App\Jobs\LimpaActivityLogJob@handle')
-            ->weekdays()
-            ->timezone('America/Sao_Paulo')
-            ->at('09:00');
-
-//        $schedule->call('App\Http\Controllers\Gescon\Siasg\SiasgcontratoCrudController@executaJobAtualizacaoSiasgContratos')
-//            ->weekdays()
-//            ->timezone('America/Sao_Paulo')
-//            ->everyMinute();
-
-//        $schedule->call('App\Http\Controllers\Gescon\Siasg\SiasgcompraCrudController@executaJobAtualizacaoSiasgCompras')
-//            ->weekdays()
-//            ->timezone('America/Sao_Paulo')
-//            ->everyMinute();
-
-
+        $this->criarJobs($schedule);
+        $this->executarJobs($schedule);
     }
 
     /**
@@ -88,4 +40,86 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+
+    protected function criarJobs(Schedule $schedule)
+    {
+//        $this->jobAtualizarSfPadrao($schedule);
+        $this->jobEnviarEmailsAlertas($schedule);
+//        $this->jobAtualizarEmpenhos($schedule);
+//        $this->jobMigrarEmpenhos($schedule);
+//        $this->jobAtualizarSaldoDeEmpenhos($schedule);
+//        $this->jobLimparActivityLogs($schedule);
+    }
+
+    protected function executarJobs(Schedule $schedule)
+    {
+//        $this->jobAtualizarSfPadrao($schedule);
+//        $this->jobEnviarEmailsAlertas($schedule);
+//        $this->jobAtualizarEmpenhos($schedule);
+//        $this->jobMigrarEmpenhos($schedule);
+//        $this->jobAtualizarSaldoDeEmpenhos($schedule);
+//        $this->jobLimparActivityLogs($schedule);
+    }
+
+    protected function jobAtualizarSfPadrao(Schedule $schedule)
+    {
+        $schedule->call(
+            'App\Http\Controllers\Gescon\ContratosfpadraoCrudController@executaJobAtualizacaoSfPadrao'
+        )
+            ->timezone('America/Sao_Paulo')
+            ->weekdays()
+            ->everyMinute();
+    }
+
+    protected function jobEnviarEmailsAlertas(Schedule $schedule)
+    {
+        $schedule->call(
+            'App\Http\Controllers\Admin\AlertaContratoController@enviaEmails'
+        )
+            ->timezone('America/Sao_Paulo')
+            // ->dailyAt('08:00')
+            ->everyMinute();
+    }
+
+    protected function jobAtualizarEmpenhos(Schedule $schedule)
+    {
+        $schedule->call(
+            'App\Http\Controllers\Execfin\EmpenhoCrudController@executaAtualizacaoNd'
+        )
+            ->timezone('America/Sao_Paulo')
+            ->weekdays()
+            ->at('08:30');
+    }
+
+    protected function jobMigrarEmpenhos(Schedule $schedule)
+    {
+        $schedule->call(
+            'App\Http\Controllers\Execfin\EmpenhoCrudController@executaMigracaoEmpenho'
+        )
+            ->timezone('America/Sao_Paulo')
+            ->weekdays()
+            ->at('08:40');
+    }
+
+    protected function jobAtualizarSaldoDeEmpenhos(Schedule $schedule)
+    {
+        $schedule->call(
+            'App\Http\Controllers\Execfin\EmpenhoCrudController@executaAtualizaSaldosEmpenhos'
+        )
+            ->timezone('America/Sao_Paulo')
+            ->weekdays()
+            ->at('08:50');
+    }
+
+    protected function jobLimparActivityLogs(Schedule $schedule)
+    {
+        $schedule->call(
+            'App\Jobs\LimpaActivityLogJob@handle'
+        )
+            ->timezone('America/Sao_Paulo')
+            ->weekdays()
+            ->at('09:00');
+    }
+
+
 }
