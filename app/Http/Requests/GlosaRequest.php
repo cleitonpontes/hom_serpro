7052931131c7a0abc685d2da2d4b475e9e7ba6f3
Saulo Soares <saulosao@gmail.com>
2020-09-29 16:57:40 -0300

    commit IMR

--- app/Http/Requests/GlosaRequest.php
+++ app/Http/Requests/GlosaRequest.php
@@ -26,7 +26,8 @@ class GlosaRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+             'valor_glosa' => 'required',
+             'escopo_id' => 'required'
         ];
     }
 
--- resources/views/vendor/backpack/crud/fields/slider.blade.php
+++ resources/views/vendor/backpack/crud/fields/slider.blade.php
@@ -1,18 +1,31 @@
-<!-- html5 range input -->
+<!-- ion-rangeslider 2.3.1 slider -->
+
+@php
+    $old = old(square_brackets_to_dots($field['name']));
+
+    $from = 0;
+    $to = 100;
+
+    if ($old){
+        $from = substr($old,0,strpos($old,';'));
+        $to = substr($old,strpos($old,';')+1);
+    } elseif  (isset($entry)) {
+        $from = ($entry->from);
+        $to = ($entry->to);
+    }
+@endphp
 <div @include('crud::inc.field_wrapper_attributes') >
     <label>{!! $field['label'] !!}</label>
     @include('crud::inc.field_translatable_icon')
-{{--    <input--}}
-{{--        type="range"--}}
-{{--        name="{{ $field['name'] }}"--}}
-{{--        value="{{ old(square_brackets_to_dots($field['name'])) ?? $field['value'] ?? $field['default'] ?? '' }}"--}}
-{{--        @include('crud::inc.field_attributes')--}}
-{{--        >--}}
-
-    <input type="text" id="example_id" name="slider" value=""
-{{--        @include('crud::inc.field_attributes')--}}
+    <input type="text" class="js-range-slider" name="{{ $field['name'] }}" value=""
+           data-type="double"
+           data-min="{{ $field['min'] }}"
+           data-max="{{ $field['max'] }}"
+           data-step="{{ $field['step'] }}"
+           data-from="{{ $from }}"
+           data-to="{{ $to }}"
+           data-grid="{{ $field['grid'] }}"
     />
-
 
     {{-- HINT --}}
     @if (isset($field['hint']))
@@ -28,29 +41,19 @@
     {{-- FIELD JS - will be loaded in the after_scripts section --}}
     @push('crud_fields_scripts')
         <!--Plugin CSS file with desired skin-->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/css/ion.rangeSlider.min.css"/>
+        <link rel="stylesheet"
+              href="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/css/ion.rangeSlider.min.css"/>
 
         <!--jQuery-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+{{--        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>--}}
 
         <!--Plugin JavaScript file-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/js/ion.rangeSlider.min.js"></script>
 
-
-
         <script src="{{ asset('vendor/adminlte/bower_components/select2/dist/js/select2.min.js') }}"></script>
         <script>
-            jQuery(document).ready(function($) {
-                $("#example_id").ionRangeSlider({
-                    grid: true,
-                    type: "double",
-                    step: "0.1"
-                });
-            });
+            $(".js-range-slider").ionRangeSlider();
         </script>
     @endpush
-
-
-
 
 @endif
