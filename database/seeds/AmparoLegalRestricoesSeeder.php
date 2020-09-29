4827b33f910c700fb0aaa9595eca4f20b259f08d
Anderson Sathler <asathler@gmail.com>
2020-09-29 15:13:22 -0300

    minor

--- database/seeds/AmparoLegalRestricoesSeeder.php
+++ database/seeds/AmparoLegalRestricoesSeeder.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Database\Seeder;
 
-// composer require laracasts/testdummy
-use Laracasts\TestDummy\Factory as TestDummy;
-
 class AmparoLegalRestricoesSeeder extends Seeder
 {
     public function run()
