e17053aa50dc24bb639d89b085cd76efc6b2ad7f
willian <willyan00@gmail.com>
2020-11-06 16:33:06 -0300

    Renomeando migration Minutaempenhos coluna valortotal

--- database/migrations/2020_10_28_233920_alter_minutaempenhos_valortotal_table.php
+++ database/migrations/2020_10_28_233920_alter_minutaempenhos_valortotal_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterMinutaempenhosTable2 extends Migration
+class AlterMinutaempenhosValortotalTable extends Migration
 {
     /**
      * Run the migrations.
