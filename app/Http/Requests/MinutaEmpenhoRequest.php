3109ba35c691805c0997b2e580a3a1ce9719b100
willian <willyan00@gmail.com>
2020-11-18 09:42:22 -0300

    correcao

--- app/Http/Requests/MinutaEmpenhoRequest.php
+++ app/Http/Requests/MinutaEmpenhoRequest.php
@@ -33,7 +33,7 @@ class MinutaEmpenhoRequest extends FormRequest
             'descricao'=> 'required|max:468',
 //            'local_entrega'=> 'required',
             'taxa_cambio'=> 'required',
-            'amparo_legal_id' => 'require',
+            'amparo_legal_id' => 'required',
             'processo' => 'max:20',
             'data_emissao' => "date|before_or_equal:{$this->data_hoje}",
 
