ac48d6410580902cf88c64effe055e6c29b823ce
Heles <helesjunior@gmail.com>
2020-11-18 21:36:19 -0300

    Ajuste Api Unidade 2

--- app/Http/Controllers/Api/UnidadeController.php
+++ app/Http/Controllers/Api/UnidadeController.php
@@ -18,6 +18,14 @@ class UnidadeController extends Controller
         // se for passado na consulta o id do orgão será filtrado pelo id e pela serarch_term
         if ($search_term)
         {
+            if ($form['orgao_id']) {
+                $options = Unidade::whereHas('orgao', function ($q) use ($form) {
+                    $q->where('id', $form['orgao_id']);
+                })
+                    ->where('codigo', 'LIKE', '%' . strtoupper($search_term) . '%');
+
+                return $options->paginate(10);
+            }
 
             $results = Unidade::where('codigo', 'LIKE', '%'.strtoupper($search_term).'%')
                 ->orWhere('nome', 'LIKE', '%'.strtoupper($search_term).'%')
@@ -28,6 +36,8 @@ class UnidadeController extends Controller
         {
             $results = Unidade::paginate(10);
         }
+
+        return $results;
 
     }
 
