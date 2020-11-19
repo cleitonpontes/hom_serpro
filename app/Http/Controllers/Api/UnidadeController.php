5f7934fddcd34420b76977db2eecea58dd2a26ee
Heles <helesjunior@gmail.com>
2020-11-19 13:02:38 -0300

    Ajuste Bug Unidades API

--- app/Http/Controllers/Api/UnidadeController.php
+++ app/Http/Controllers/Api/UnidadeController.php
@@ -13,32 +13,26 @@ class UnidadeController extends Controller
     public function index(Request $request)
     {
         $search_term = $request->input('q');
-        $form = collect($request->input('form'))->pluck('value', 'name');
+        $page = $request->input('page');
 
-        if ($search_term) {
-            // se for passado na consulta o id do orgão será filtrado pelo id e pela serarch_term
-            if (isset($form['orgao_id'])) {
-                $results = Unidade::whereHas('orgao', function ($q) use ($form) {
-                    $q->where('id', $form['orgao_id']);
-                })
-                    ->where('codigo', 'LIKE', '%' . strtoupper($search_term) . '%')
-                    ->paginate(10);
-            }else{
-                $results = Unidade::where('codigo', 'LIKE', '%' . strtoupper($search_term) . '%')
-                    ->orWhere('nome', 'LIKE', '%' . strtoupper($search_term) . '%')
-                    ->orWhere('nomeresumido', 'LIKE', '%' . strtoupper($search_term) . '%')
-                    ->paginate(10);
-            }
-        } else {
+        if ($search_term)
+        {
+            $results = Unidade::where('codigo', 'LIKE', '%'.strtoupper($search_term).'%')
+                ->orWhere('nome', 'LIKE', '%'.strtoupper($search_term).'%')
+                ->orWhere('nomeresumido', 'LIKE', '%'.strtoupper($search_term).'%')
+                ->paginate(10);
+        }
+        else
+        {
             $results = Unidade::paginate(10);
         }
 
         return $results;
-
     }
 
     public function show($id)
     {
         return Unidade::find($id);
     }
+
 }
