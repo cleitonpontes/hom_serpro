1c2e2272e7ce3831c7083dfa31d5731351f0f301
Adriano <adriano.msx8@gmail.com>
2020-11-26 16:33:19 -0300

    Documento 1.3
    consumo de api exemplo

--- app/Console/Commands/SanitizarComprasContratos.php
+++ app/Console/Commands/SanitizarComprasContratos.php
@@ -42,6 +42,13 @@ class SanitizarComprasContratos extends Command
            * -> Sanitizar dados da tabela (contratoitens) de acordo com a API (ContratoSiasg)
         */
 
+        $url = "https://swapi.dev/api/people/";
+        $ch = curl_init($url);    
+        curl_setopt($ch , CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch , CURLOPT_SSL_VERIFYPEER, false);
+        $resultado = json_decode(curl_exec($ch));
+        var_dump($resultado);
+
         /**
          * passo 2
          * percorrer os contratos 
