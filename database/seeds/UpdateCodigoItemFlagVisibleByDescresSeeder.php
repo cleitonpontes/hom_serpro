d38465cbf63c0b55d6c96be2f8aaed85645b0694
Franklin <franklin.linux@gmail.com>
2020-12-09 15:22:37 -0300

    commit alter seed

--- database/seeds/UpdateCodigoItemFlagVisibleByDescresSeeder.php
+++ database/seeds/UpdateCodigoItemFlagVisibleByDescresSeeder.php
@@ -7,13 +7,12 @@ class UpdateCodigoItemFlagVisibleByDescresSeeder extends Seeder
 {
     public function run()
     {
-        $codigoitem = Codigoitem::whereRaw('LENGTH(descres) <= 2')
-                             ->where('codigo_id', '=', 13)
-                             ->orWhere('descres', '=', 'NAOSEAPLIC')
-                             ->update(['visivel' => true]);
-
         $codigoitem = Codigoitem::whereRaw('LENGTH(descres) > 2')
-                           ->where('descres', '<>', 'NAOSEAPLIC')
-                          ->update(['visivel' => false]);
+                             ->where('codigo_id', '=', 13)
+                             ->update(['visivel' => false]);
+
+        $codigoitem = Codigoitem::where('codigo_id', '=', 13)
+                           ->where('descres', '=', 'NAOSEAPLIC')
+                          ->update(['visivel' => true]);
     }
 }
