ec11412b0c07165e361743ba3018b11185a6795d
Adriano <adriano.msx8@gmail.com>
2020-12-09 11:51:33 -0300

    ajustes na seeder codigo itens

--- database/seeds/UpdateCodigoItemFlagVisibleByDescresSeeder.php
+++ database/seeds/UpdateCodigoItemFlagVisibleByDescresSeeder.php
@@ -7,8 +7,15 @@ class UpdateCodigoItemFlagVisibleByDescresSeeder extends Seeder
 {
     public function run()
     {
-        $codigoitem = Codigoitem::whereRaw('LENGTH(descres) > 2')
-                            ->where('descres', '<>', 'NAOSEAPLIC')
-        ->update(['visivel' => false]);
+        $codigoitem = Codigoitem::whereRaw('LENGTH(descres) = 2')
+                             ->where('codigo_id', '=', 13)
+                             ->orWhere('descres', '=', 'NAOSEAPLIC')
+        ->update(['visivel' => true]);
+
+        $codigoitem = Codigoitem::whereRaw('LENGTH(descres) <> 2')
+                          ->where('codigo_id', '<>', 13)
+                          ->update(['visivel' => false]);
+    
+
     }
 }
