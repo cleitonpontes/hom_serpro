7f4a88f121e7318f5500fb706ca26aaba1f34d46
Heles <helesjunior@gmail.com>
2020-12-13 17:05:37 -0300

    Ajuste 3

--- app/Http/Traits/Busca.php
+++ app/Http/Traits/Busca.php
@@ -11,10 +11,9 @@ trait Busca
         curl_setopt($ch, CURLOPT_TIMEOUT, 180);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 180);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13");
         curl_setopt($ch, CURLOPT_URL, $url);
         $data = curl_exec($ch);
-        $data = utf8_encode($data);
+
         curl_close($ch);
 
         return json_decode($data, true);
