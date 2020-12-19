71ab50cf32cdce2938ac86003d552de71bb4308c
Heles <helesjunior@gmail.com>
2020-12-19 17:00:09 -0300

    Commit

--- app/Http/Controllers/Api/ComprasnetController.php
+++ app/Http/Controllers/Api/ComprasnetController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Traits\Formatador;
 use App\Models\Codigoitem;
+use App\Models\Contrato;
 use App\Models\Contratoitem;
 use App\Models\Unidade;
 use Illuminate\Http\Request;
@@ -21,38 +22,104 @@ class ComprasnetController extends Controller
             return $retorno;
         }
 
-        $dados['uasg'] = $request->uasg;
-        $dados['modalidade'] = $request->modalidade;
-        $dados['numero'] = $request->numero;
-        $dados['ano'] = $request->ano;
-        $dados['itens'] = $request->itens;
+        $dados['uasg'] = @$request->uasg;
+        $dados['modalidade'] = @$request->modalidade;
+        $dados['numeroAno'] = @$request->numero . '/' . @$request->ano;
+        $dados['itens'] = $this->trataItens($request->itens);
 
-        $retorno = [
-            'itens' => [
-                [
-                    'nroItem' => '0001',
-                    'contratosAtivos' => [
-                        '11016150000012019000000',
-                        '11062150000022019000000',
-                    ],
-                    'empenhos' => [
-                        '110161000012020NE000001',
-                        '110621000012020NE000001',
-                    ],
-                ],
-                [
-                    'nroItem' => '0002',
-                    'contratosAtivos' => [
+        $unidade = $this->buscaUnidadePorCodigo($dados['uasg']);
+        $modalidade = $this->buscaModalidadePorCodigo($dados['modalidade']);
 
-                    ],
-                    'empenhos' => [
+        if (isset($unidade->id) and isset($modalidade->id)) {
 
-                    ],
-                ],
-            ]
-        ];
+            $retorno['itens'] = [];
+
+            foreach ($dados['itens'] as $item) {
+                $num_item = str_pad($item, 5, "0", STR_PAD_LEFT);
+
+                $contratos = $this->buscaContratosItemUnidadeCompra($num_item, $modalidade->id, $unidade->id, $dados['numeroAno']);
+
+                $array_contratos = [];
+                foreach ($contratos as $contrato){
+                    $unidadeorigem = $contrato->unidadeorigem->codigo;
+                    $unidadeatual = $contrato->unidade->codigo;
+                    $unidadesubrrogacao = ($unidadeorigem == $unidadeatual) ? '000000' : $unidadeatual;
+                    $tipo = $contrato->tipo->descres;
+                    $numero_contrato = str_replace('/' , '', $contrato->numero);
+
+                    $array_contratos[] = $unidadeorigem . $tipo . $numero_contrato . $unidadesubrrogacao;
+                }
+
+                $array_empenhos = [];
+
+
+
+                $retorno['itens'][] = [
+                    'nroItem' => $num_item,
+                    'contratosAtivos' => $array_contratos,
+                    'empenhos' => $array_empenhos
+                ];
+
+            }
+
+//            $retorno = [
+//                'itens' => [
+//                    [
+//                        'nroItem' => '0001',
+//                        'contratosAtivos' => [
+//                            '11016150000012019000000',
+//                            '11062150000022019000000',
+//                        ],
+//                        'empenhos' => [
+//                            '110161000012020NE000001',
+//                            '110621000012020NE000001',
+//                        ],
+//                    ],
+//                    [
+//                        'nroItem' => '0002',
+//                        'contratosAtivos' => [
+//
+//                        ],
+//                        'empenhos' => [
+//
+//                        ],
+//                    ],
+//                ]
+//            ];
+        }
+
 
         return $retorno;
+    }
+
+    private function buscaContratosItemUnidadeCompra(string $item, int $modalidade, int $unidade, string $numeroAnoCompra)
+    {
+        $contratos = Contrato::whereHas('itens', function ($i) use ($item) {
+            $i->where('numero_item_compra', $item);
+        })
+            ->where('modalidade_id', $modalidade)
+            ->where('unidadecompra_id', $unidade)
+            ->where('licitacao_numero', $numeroAnoCompra);
+
+        return $contratos->get();
+
+    }
+
+    private function trataItens(string $itens)
+    {
+        $array = [];
+
+        if($itens){
+            $itens = str_replace(']', '', str_replace('[', '', $itens));
+
+            if (strpos($itens, ',') !== false) {
+                $array = explode(',', $itens);
+            } else {
+                $array[] = $itens;
+            }
+        }
+
+        return $array;
     }
 
     public function getDadosContratosPorItem(Request $request)
@@ -79,25 +146,25 @@ class ComprasnetController extends Controller
 
         if (isset($unidade_compra->id) and isset($modalidade->id)) {
 
-            $dados = Contratoitem::whereHas('contrato', function ($q) use ($dados, $unidade_compra,$modalidade, $unidade_contrato){
-                $q->where('unidadecompra_id',$unidade_compra->id)
-                    ->where('modalidade_id',$modalidade->id)
-                    ->where('licitacao_numero',$dados['numeroAnoCompra']);
-                if(isset($unidade_contrato->id)){
-                    $q->where('unidade_id',$unidade_contrato->id);
+            $dados = Contratoitem::whereHas('contrato', function ($q) use ($dados, $unidade_compra, $modalidade, $unidade_contrato) {
+                $q->where('unidadecompra_id', $unidade_compra->id)
+                    ->where('modalidade_id', $modalidade->id)
+                    ->where('licitacao_numero', $dados['numeroAnoCompra']);
+                if (isset($unidade_contrato->id)) {
+                    $q->where('unidade_id', $unidade_contrato->id);
                 }
-                if($dados['fornecedor']){
-                    $q->whereHas('fornecedor', function ($f) use($dados){
-                        $f->where('cpf_cnpj_idgener',$this->formataCnpjCpf($dados['fornecedor']));
+                if ($dados['fornecedor']) {
+                    $q->whereHas('fornecedor', function ($f) use ($dados) {
+                        $f->where('cpf_cnpj_idgener', $this->formataCnpjCpf($dados['fornecedor']));
                     });
                 }
             })
-                ->where('numero_item_compra',$dados['item_compra'])
+                ->where('numero_item_compra', $dados['item_compra'])
                 ->get();
 
 
-            foreach ($dados as $dado){
-                $instrumento_inicial = $dado->contrato->historico()->whereHas('tipo', function ($t){
+            foreach ($dados as $dado) {
+                $instrumento_inicial = $dado->contrato->historico()->whereHas('tipo', function ($t) {
                     $t->where('descricao', '<>', 'Termo Aditivo')
                         ->where('descricao', '<>', 'Termo de Apostilamento')
                         ->where('descricao', '<>', 'Termo de Rescisão');
@@ -111,7 +178,7 @@ class ComprasnetController extends Controller
                     'fornecedor' => @$dado->contrato->fornecedor->cpf_cnpj_idgener,
                     'vigencia_fim_inicial' => @$instrumento_inicial->vigencia_fim,
                     'vigencia_fim' => @$dado->contrato->vigencia_fim,
-                    'quantidade_item' => @number_format($dado->quantidade,0,'',''),
+                    'quantidade_item' => @number_format($dado->quantidade, 0, '', ''),
                     'valor_unitario_item' => @$dado->valorunitario,
                     'valor_total_item' => @$dado->valortotal,
                     'situacao_publicacao' => 'PUBLICADO'
