931f6bf72a87487189e06fceb5fb16f0edbd312b
Franklin <franklin.linux@gmail.com>
2020-12-19 18:01:28 -0300

    ajuste campos valor em contrato e contratohistorico

---
+++ database/migrations/2020_12_19_204029_alter_campos_valor_contrato.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterCamposValorContrato extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contratos', function (Blueprint $table) {
+            $table->decimal('valor_inicial',19,4)->default(0)->change();
+            $table->decimal('valor_global',19,4)->default(0)->change();
+            $table->decimal('valor_parcela',19,4)->default(0)->change();
+            $table->decimal('valor_acumulado',19,4)->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contratos', function (Blueprint $table) {
+            $table->decimal('valor_inicial',17,2)->default(0)->change();
+            $table->decimal('valor_global',17,2)->default(0)->change();
+            $table->decimal('valor_parcela',17,2)->default(0)->change();
+            $table->decimal('valor_acumulado',17,2)->default(0)->change();
+        });
+    }
+}
