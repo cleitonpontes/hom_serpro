2e7dea021b1172f4be9acd3bf691de72b6aba297
Marcio L Vasconcelos Donato <mvascs@gmail.com>
2020-12-21 09:20:21 -0300

    Criado crud para tabela jobs

--- app/Http/Controllers/Admin/JobsCrudController.php
+++ app/Http/Controllers/Admin/JobsCrudController.php
@@ -16,16 +16,37 @@ use Backpack\CRUD\CrudPanel;
  */
 class JobsCrudController extends CrudController
 {
+
     public function setup()
     {
+
+        if(!backpack_user()->hasRole('Administrador')){
+            abort('403', config('app.erro_permissao'));
+        }
+
         /*
         |--------------------------------------------------------------------------
         | CrudPanel Basic Information
         |--------------------------------------------------------------------------
         */
         $this->crud->setModel('App\Models\Jobs');
-        $this->crud->setRoute(config('backpack.base.route_prefix') . '/jobs');
+        $this->crud->setRoute(config('backpack.base.route_prefix') . '/admin/jobs');
         $this->crud->setEntityNameStrings('jobs', 'jobs');
+
+        $this->crud->addClause('orderby', 'created_at', 'DESC');
+
+        backpack_user()->hasRole('Administrador') ? $this->crud->allowAccess('show') : $this->crud->denyAccess('show');
+        $this->crud->denyAccess('create');
+        $this->crud->denyAccess('update');
+        $this->crud->denyAccess('delete');
+        // $this->crud->allowAccess('show');
+
+        $this->crud->enableExportButtons();
+
+        // colunas da listagem
+        $colunas = $this->Colunas();
+        $this->crud->addColumns($colunas);
+
 
         /*
         |--------------------------------------------------------------------------
@@ -34,11 +55,148 @@ class JobsCrudController extends CrudController
         */
 
         // TODO: remove setFromDb() and manually define Fields and Columns
-        $this->crud->setFromDb();
+        // $this->crud->setFromDb();
 
         // add asterisk for fields that are required in JobsRequest
         $this->crud->setRequiredFields(StoreRequest::class, 'create');
         $this->crud->setRequiredFields(UpdateRequest::class, 'edit');
+    }
+
+    public function Colunas()
+    {
+
+        $colunas = [
+
+            [
+                'name'  => 'queue',
+                'label' => 'Queue',
+                'type'  => 'text',
+                'limit' => 9999,
+                'orderable' => true,
+                'visibleInTable' => false, // no point, since it's a large text
+                'visibleInModal' => true, // would make the modal too big
+                'visibleInExport' => true, // not important enough
+                'visibleInShow' => true, // sure, why not
+                // 'searchLogic' => function (Builder $query, $column, $searchTerm) {
+                //     $query->orWhere('feriados.data', 'ilike', "%$searchTerm%");
+                // },
+            ],
+            [
+                'name'  => 'payload',
+                'label' => 'Payload',
+                'type'  => 'text',
+                'limit' => 9999,
+                'orderable' => true,
+                'visibleInTable' => false, // no point, since it's a large text
+                'visibleInModal' => true, // would make the modal too big
+                'visibleInExport' => true, // not important enough
+                'visibleInShow' => true, // sure, why not
+                // 'searchLogic' => function (Builder $query, $column, $searchTerm) {
+                //     $query->orWhere('feriados.data', 'ilike', "%$searchTerm%");
+                // },
+            ],
+            [
+                'name'  => 'attempts',
+                'label' => 'Attempts',
+                'type'  => 'text',
+                'limit' => 9999,
+                'orderable' => true,
+                'visibleInTable' => true, // no point, since it's a large text
+                'visibleInModal' => true, // would make the modal too big
+                'visibleInExport' => true, // not important enough
+                'visibleInShow' => true, // sure, why not
+                // 'searchLogic' => function (Builder $query, $column, $searchTerm) {
+                //     $query->orWhere('feriados.data', 'ilike', "%$searchTerm%");
+                // },
+            ],
+            [
+                'name'  => 'reserved_at',
+                'label' => 'Reserved at',
+                'type'  => 'text',
+                'orderable' => true,
+                'visibleInTable' => true, // no point, since it's a large text
+                'visibleInModal' => true, // would make the modal too big
+                'visibleInExport' => true, // not important enough
+                'visibleInShow' => true, // sure, why not
+                // 'searchLogic' => function (Builder $query, $column, $searchTerm) {
+                //     $query->orWhere('feriados.data', 'ilike', "%$searchTerm%");
+                // },
+            ],
+            [
+                'name'  => 'available_at',
+                'label' => 'Available at',
+                'type'  => 'text',
+                'orderable' => true,
+                'visibleInTable' => true, // no point, since it's a large text
+                'visibleInModal' => true, // would make the modal too big
+                'visibleInExport' => true, // not important enough
+                'visibleInShow' => true, // sure, why not
+                // 'searchLogic' => function (Builder $query, $column, $searchTerm) {
+                //     $query->orWhere('feriados.data', 'ilike', "%$searchTerm%");
+                // },
+            ],
+            [
+                'name'  => 'created_at',
+                'label' => 'Created at',
+                'type'  => 'text',
+                'orderable' => true,
+                'visibleInTable' => true, // no point, since it's a large text
+                'visibleInModal' => true, // would make the modal too big
+                'visibleInExport' => true, // not important enough
+                'visibleInShow' => true, // sure, why not
+                // 'searchLogic' => function (Builder $query, $column, $searchTerm) {
+                //     $query->orWhere('feriados.data', 'ilike', "%$searchTerm%");
+                // },
+            ],
+
+
+
+
+            // [
+            //     'name'  => 'connection',
+            //     'label' => 'Connection',
+            //     'type'  => 'text',
+            //     'orderable' => true,
+            //     'visibleInTable' => true, // no point, since it's a large text
+            //     'visibleInModal' => true, // would make the modal too big
+            //     'visibleInExport' => true, // not important enough
+            //     'visibleInShow' => true, // sure, why not
+            //     // 'searchLogic' => function (Builder $query, $column, $searchTerm) {
+            //     //     $query->orWhere('feriados.data', 'ilike', "%$searchTerm%");
+            //     // },
+            // ],
+            // [
+            //     'name'  => 'queue',
+            //     'label' => 'Queue',
+            //     'type'  => 'text',
+            //     'orderable' => true,
+            //     'visibleInTable' => true, // no point, since it's a large text
+            //     'visibleInModal' => true, // would make the modal too big
+            //     'visibleInExport' => true, // not important enough
+            //     'visibleInShow' => true, // sure, why not
+            //     // 'searchLogic' => function (Builder $query, $column, $searchTerm) {
+            //     //     $query->orWhere('feriados.data', 'ilike', "%$searchTerm%");
+            //     // },
+            // ],
+
+
+            // [
+            //     'name'  => 'exception',
+            //     'label' => 'Exception',
+            //     'type'  => 'text',
+            //     'limit' => 9999,
+            //     'orderable' => true,
+            //     'visibleInTable' => false, // no point, since it's a large text
+            //     'visibleInModal' => true, // would make the modal too big
+            //     'visibleInExport' => true, // not important enough
+            //     'visibleInShow' => true, // sure, why not
+            //     // 'searchLogic' => function (Builder $query, $column, $searchTerm) {
+            //     //     $query->orWhere('exception', 'ilike', "%$searchTerm%");
+            //     // },
+            // ],
+
+        ];
+        return $colunas;
     }
 
     public function store(StoreRequest $request)
--- app/Models/Jobs.php
+++ app/Models/Jobs.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Backpack\CRUD\CrudTrait;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Jobs extends Model
 {
@@ -19,7 +21,14 @@ class Jobs extends Model
     // protected $primaryKey = 'id';
     // public $timestamps = false;
     // protected $guarded = ['id'];
-    protected $fillable = [];
+    protected $fillable = [
+        'queue',
+        'payload',
+        'attempts',
+        'reserved_at',
+        'available_at',
+        'created_at'
+    ];
     // protected $hidden = [];
     // protected $dates = [];
 
