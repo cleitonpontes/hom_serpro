ac8dea2edfbe789f6533992dac805b1a4f5a648d
Heles junior <helesjunior@gmail.com>
2020-12-24 10:20:22 -0300

    Teste

--- app/Http/Controllers/Empenho/CompraSiasgCrudController.php
+++ app/Http/Controllers/Empenho/CompraSiasgCrudController.php
@@ -242,6 +242,7 @@ class CompraSiasgCrudController extends CrudController
         if ($tipoCompra == $this::SISPP) {
             if ($subrrogada != '000000') {
                 ($subrrogada == session('user_ug')) ? $unidade_autorizada_id = session('user_ug_id') : '';
+                dd($unidade_autorizada_id, session('user_ug'), session('user_ug_id'));
             } else {
                 ($request->unidade_origem_id == session('user_ug_id'))
                     ? $unidade_autorizada_id = $request->unidade_origem_id : '';
