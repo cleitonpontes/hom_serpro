e7a092a84be2b95fa03bba34e758b73cd52c12d9
Heles junior <helesjunior@gmail.com>
2020-12-26 11:44:16 -0300

    Atualização

--- app/Http/Requests/PadroespublicacaoRequest.php
+++ app/Http/Requests/PadroespublicacaoRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Unique;
 
 class PadroespublicacaoRequest extends FormRequest
 {
@@ -25,8 +26,16 @@ class PadroespublicacaoRequest extends FormRequest
      */
     public function rules()
     {
+        $id = $this->id ?? "NULL";
+        $tipo_mudanca_id = $this->tipo_mudanca_id ?? "NULL";
+
         return [
-            'tipo_contrato_id' => 'required',
+            'tipo_contrato_id' => [
+                'required',
+                (new Unique('padroespublicacao', 'tipo_contrato_id'))
+                    ->ignore($id)
+                    ->where('tipo_mudanca_id', $tipo_mudanca_id)
+            ],
             'tipo_mudanca_id' => 'required',
             'texto_padrao' => 'required',
             'identificador_norma_id' => 'required',
@@ -41,7 +50,10 @@ class PadroespublicacaoRequest extends FormRequest
     public function attributes()
     {
         return [
-            //
+            'tipo_contrato_id' => 'Tipo Instrumento',
+            'tipo_mudanca_id' => 'Tipo Mudança',
+            'identificador_norma_id' => 'Identificador Norma',
+            'texto_padrao' => 'Texto Padrão',
         ];
     }
 
