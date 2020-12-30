f1da1fe2da3a5185fc1107cd66d03e1ed2942411
Heles junior <helesjunior@gmail.com>
2020-12-30 17:30:53 -0300

    Ajuste

--- app/Http/Traits/Formatador.php
+++ app/Http/Traits/Formatador.php
@@ -71,7 +71,7 @@ trait Formatador
 
     public function formataIntengerSiasg($dado)
     {
-        return number_format($dado,0);
+        return number_format($dado,0,'');
     }
 
     public function formataNumeroContratoLicitacao($dado): string
