d0dbfa833383e1ff1e1a4c59504e6d07413bb125
Luiz Mendanha <luiz.mendanha@agu.gov.br>
2021-01-02 11:40:18 -0300

    primeiro commit

---
+++ public/js/mensagem/confirmacaoPublicacao.js
@@ -0,0 +1,31 @@
+$(document).on('click', "#btn-submit-itens-contrato", function () {
+
+    var tipo_contrato = $('#tipo_contrato').val();
+    if($( "[name='tipo_id']" ).length === 1){
+        var selected = $("[name='tipo_id']").find(':selected'),
+            array_selected = [];
+        selected.each(function (index, option) {
+            array_selected[index] = option.text;
+        })
+        if (array_selected[0] !== 'Selecione...') {
+            tipo_contrato = array_selected[0];
+        }
+    }
+
+
+    if (tipo_contrato === 'Outros' || tipo_contrato === 'Empenho') {
+        Swal.fire({
+            title: 'O instrumento será publicado, deseja continuar?',
+            showDenyButton: false,
+            showCancelButton: true,
+            confirmButtonText: `Sim`,
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                $('form').submit();
+            }
+        })
+    } else {
+        $('form').submit();
+    }
+});
