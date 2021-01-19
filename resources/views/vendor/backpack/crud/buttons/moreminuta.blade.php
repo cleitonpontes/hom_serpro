9438783799182b30df63a0317fe7e88f91b405ad
Saulo <saulosao@gmail.com>
2021-01-19 12:02:40 -0300

    minuta de alteracao somente para minutas que estejam com a situacao 'EMPENHO EMITIDO'

--- resources/views/vendor/backpack/crud/buttons/moreminuta.blade.php
+++ resources/views/vendor/backpack/crud/buttons/moreminuta.blade.php
@@ -1,11 +1,22 @@
+    @php
+        $url = '';
+        $disabled = 'disabled';
+        if ($entry->situacao_descricao === 'EMPENHO EMITIDO'){
+            $url = "/empenho/minuta/{$entry->getKey()}/alteracao";
+            $disabled = '';
+        }
+    @endphp
 <div class="btn-group">
     <button type="button" title="Mais" class="btn btn-xs btn-default dropdown-toggle dropdown-toggle-split"
-            data-toggle="dropdown"  aria-haspopup="true" aria-expanded="false" title="Mais"><i class="fa fa-gears"></i>
+            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title="Mais"><i class="fa fa-gears"></i>
         <span class="caret"></span>
         <span class="sr-only">Toggle Dropdown</span>
     </button>
-    <ul class="dropdown-menu dropdown-menu-right">
-        {{--        <li class="dropdown-header"><i class="fa fa-list"></i> Itens Contrato:</li>--}}
-        <li><a href="/empenho/minuta/{{$entry->getKey()}}/alteracao">&nbsp;&nbsp;&nbsp;<i class="fa fa-angle-right"></i>Alterar Empenho</a></li>
-    </ul>
+        <ul class="dropdown-menu dropdown-menu-right">
+            <li class="{{$disabled}}">
+                <a href="{{$url}}" {{$disabled}}>&nbsp;&nbsp;&nbsp;
+                    <i class="fa fa-angle-right"></i>Alterar Empenho</a>
+            </li>
+        </ul>
+
 </div>
