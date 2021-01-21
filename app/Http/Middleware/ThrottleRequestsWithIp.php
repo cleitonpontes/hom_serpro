bb99300638694355fe2cebfd4b48292daae802ac
michael henrique <michael.rocha@agu.gov.br>
2021-01-21 14:56:40 -0300

    comit

--- app/Http/Middleware/ThrottleRequestsWithIp.php
+++ app/Http/Middleware/ThrottleRequestsWithIp.php
@@ -17,9 +17,28 @@ class ThrottleRequestsWithIp extends \Illuminate\Routing\Middleware\ThrottleRequ
      */
     public function handle($request, Closure $next, $maxAttempts = 60, $decayMinutes = 1, $prefix = '')
     {
-        $ipsCadastrado = Ipsacesso::whereJsonContains('ips', [['name' =>$request->ip()]])->get()->toArray();
 
-        if(empty($ipsCadastrado)) {
+        $ipurl = '207.64.1.64';
+//        207.64.1.68/28
+        $todosIps = [];
+        foreach(Ipsacesso::all() as $ipsJson) {
+            $arrIps = json_decode($ipsJson->ips);
+            foreach($arrIps as $ip){
+                $split = explode('/', $ip->name);
+                if (count($split) > 1) {
+                    $retornoArrIps = $this->cidrToRange($ip->name);
+                    dd($retornoArrIps);
+                    foreach($retornoArrIps as $ip){
+                        array_push($todosIps, $ip);
+                    }
+                }else{
+                    array_push($todosIps, $ip->name);
+                }
+            }
+        }
+
+//        if(!in_array($request->ip(), $todosIps)) {
+        if(!in_array($ipurl, $todosIps)) {
             abort('403', config('app.erro_permissao'));
         }
         return parent::handle($request, $next, $maxAttempts, $decayMinutes, $prefix);
