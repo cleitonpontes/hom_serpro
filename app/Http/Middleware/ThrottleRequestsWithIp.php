7704d96ca2c5cf8f0ab3aa28d76beeabcfccc341
Luiz Mendanha <luiz.mendanha@agu.gov.br>
2021-01-21 15:19:47 -0300

    ajuste na funcao cidrToRange

--- app/Http/Middleware/ThrottleRequestsWithIp.php
+++ app/Http/Middleware/ThrottleRequestsWithIp.php
@@ -49,7 +49,8 @@ class ThrottleRequestsWithIp extends \Illuminate\Routing\Middleware\ThrottleRequ
         $split = explode('/', $value);
         if (!empty($split[0]) && is_scalar($split[1]) && filter_var($split[0], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
             $rangeStart = ip2long($split[0]) & ((-1 << (32 - (int)$split[1])));
-            $rangeEnd = ip2long($split[0]) + pow(2, (32 - (int)$split[1])) - 1;
+            $rangeStartIP = long2ip($rangeStart);
+            $rangeEnd = ip2long($rangeStartIP) + pow(2, (32 - (int)$split[1])) - 1;
 
             for ($i = $rangeStart; $i <= $rangeEnd; $i++) {
                 $range[] = long2ip($i);
