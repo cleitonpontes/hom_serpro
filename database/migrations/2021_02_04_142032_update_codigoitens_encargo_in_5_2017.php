ff1c129d69c82e7d1bd0d0d0f401bef63590f31b
mvascs <mvascs@mvascss-MacBook-Pro.local>
2021-02-09 12:32:04 -0300

    migration para atualização dos dados de encargo e codigoitens dos encargos

--- database/migrations/2021_02_04_142032_update_codigoitens_encargo_in_5_2017.php
+++ database/migrations/2021_02_04_142032_update_codigoitens_encargo_in_5_2017.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 
 use App\Models\Codigo;
 use App\Models\Codigoitem;
+use App\Models\Encargo;
 
 
 class UpdateCodigoitensEncargoIn52017 extends Migration
@@ -21,7 +22,7 @@ class UpdateCodigoitensEncargoIn52017 extends Migration
         ])->first();
 
 
-        // atualização do décimo terceiro
+        // atualização do codigoitem décimo terceiro
         $codigoitem_id_decimo_terceiro = Codigoitem::where([
             'codigo_id' => $codigo->id,
             'descricao' => 'Décimo Terceiro Salário'
@@ -31,6 +32,20 @@ class UpdateCodigoitensEncargoIn52017 extends Migration
                 ['id' => $codigoitem_id_decimo_terceiro->id],   // aqui é a chave ou algum unique
                 [
                     'descricao' => '13º (décimo terceiro) salário',
+                    'updated_at' => now(),
+                ]
+            );
+        }
+        // atualização do encargo décimo terceiro
+        $tipo_id_encargo_decimo_terceiro = $codigoitem_id_decimo_terceiro->id;
+        $encargo_decimo_terceiro = Encargo::where([
+            'tipo_id' => $tipo_id_encargo_decimo_terceiro,
+        ])->first();
+        if(is_object($encargo_decimo_terceiro)){
+            Encargo::updateOrCreate(
+                ['tipo_id' => $encargo_decimo_terceiro->tipo_id],   // aqui é a chave ou algum unique
+                [
+                    'percentual' => '8.33',
                     'updated_at' => now(),
                 ]
             );
@@ -53,6 +68,21 @@ class UpdateCodigoitensEncargoIn52017 extends Migration
                 ]
             );
         }
+        // atualização do encargo férias
+        $tipo_id_encargo_ferias = $codigoitem_id_ferias->id;
+        $encargo_ferias = Encargo::where([
+            'tipo_id' => $tipo_id_encargo_ferias,
+        ])->first();
+        if(is_object($encargo_ferias)){
+            Encargo::updateOrCreate(
+                ['tipo_id' => $encargo_ferias->tipo_id],   // aqui é a chave ou algum unique
+                [
+                    'percentual' => '12.10',
+                    'updated_at' => now(),
+                ]
+            );
+        }
+
 
 
 
@@ -66,6 +96,20 @@ class UpdateCodigoitensEncargoIn52017 extends Migration
                 ['id' => $codigoitem_id_fgts->id],   // aqui é a chave ou algum unique
                 [
                     'descricao' => 'Multa sobre o FGTS para as rescisões sem justa causa',
+                    'updated_at' => now(),
+                ]
+            );
+        }
+        // atualização do encargo fgts
+        $tipo_id_encargo_fgts = $codigoitem_id_fgts->id;
+        $encargo_fgts = Encargo::where([
+            'tipo_id' => $tipo_id_encargo_fgts,
+        ])->first();
+        if(is_object($encargo_fgts)){
+            Encargo::updateOrCreate(
+                ['tipo_id' => $encargo_fgts->tipo_id],   // aqui é a chave ou algum unique
+                [
+                    'percentual' => '4.00',
                     'updated_at' => now(),
                 ]
             );
@@ -84,6 +128,20 @@ class UpdateCodigoitensEncargoIn52017 extends Migration
                 ['id' => $codigoitem_id_grupo_a->id],   // aqui é a chave ou algum unique
                 [
                     'descricao' => 'Incidência do Submódulo 2.2 sobre férias, 1/3 (um terço) constitucional de férias e 13o (décimo terceiro) salário',
+                    'updated_at' => now(),
+                ]
+            );
+        }
+        // atualização do encargo grupo a
+        $tipo_id_encargo_grupo_a = $codigoitem_id_grupo_a->id;
+        $encargo_grupo_a = Encargo::where([
+            'tipo_id' => $tipo_id_encargo_grupo_a,
+        ])->first();
+        if(is_object($encargo_grupo_a)){
+            Encargo::updateOrCreate(
+                ['tipo_id' => $encargo_grupo_a->tipo_id],   // aqui é a chave ou algum unique
+                [
+                    'percentual' => '7.82',
                     'updated_at' => now(),
                 ]
             );
