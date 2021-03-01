b49c1fdd6f927d595bfe38c7a87299a5bb86cab6
mvascs <mvascs@mvascss-MacBook-Pro.local>
2021-03-01 15:30:59 -0300

    alterada forma de mudar ug para adm

--- app/Forms/MudarUgForm.php
+++ app/Forms/MudarUgForm.php
@@ -8,22 +8,17 @@ class MudarUgForm extends Form
 {
     public function buildForm()
     {
-
-
-
+        // caso seja adm vamos abrir o campo texto para entrar com o código da UG - mvascs@gmail.com
         if (backpack_user()->hasRole('Administrador')) {
             $ugs = $this->getData('ugs') ?? "NULL";
             $this
-                ->add('ug', 'select', [
-                    'label' => 'UG/UASG',
-    //                'rules' => "required",
-                    'attr' => [
-                       'class' => 'select2_from_ajax'
-                    ],
-                    'choices' => $ugs,
-                    'empty_value' => 'Selecione',
-                ]);
-
+            ->add('ug', 'text', [
+                'label' => 'Informe o código da UG/UASG',
+                'rules' => 'required|max:6',
+                'attr' => [
+                    // 'onkeyup' => "maiuscula(this)"
+                ],
+            ]);
         } else {
             $ugs = $this->getData('ugs') ?? "NULL";
             $this
@@ -37,9 +32,5 @@ class MudarUgForm extends Form
                     'empty_value' => 'Selecione',
                 ]);
         }
-
-
-
     }
-
 }
