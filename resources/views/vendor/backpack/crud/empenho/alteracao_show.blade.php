dc207eda90cb0a1f53c44e51aba561eaec27bff8
Heles <helesjunior@gmail.com>
2021-03-12 12:00:23 -0300

    teste

--- resources/views/vendor/backpack/crud/empenho/alteracao_show.blade.php
+++ resources/views/vendor/backpack/crud/empenho/alteracao_show.blade.php
@@ -256,8 +256,12 @@
                         </button>
                     </div>
                     <div class="col-md-4" align="right">
-                        <button type="button" class="btn btn-primary" id="finalizar"
-                            {!!  (session('situacao') !== 'EMPENHO EMITIDO' || session('situacao') !== 'EM PROCESSAMENTO') ? 'disabled' : '' !!}
+                        <button type="button" class="btn btn-primary"
+                                @if (session('situacao') !== 'EMPENHO EMITIDO' || session('situacao') !== 'EM PROCESSAMENTO')
+                                id="finalizar"
+                                @else
+                                disabled
+                            @endif
                         >
                             <i class="fa fa-check-circle"></i> Finalizar
                         </button>
