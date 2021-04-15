5e5a4d3146b2d73450a06f9cfa0c1e7d0b9c4305
Luiz Mendanha <luizmendanhajr@gmail.com>
2021-04-15 07:37:31 -0300

    criado Job para importacao de terceirizado

---
+++ app/Jobs/InserirTerceirizadoEmMassaJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Http\Controllers\Admin\ImportacaoCrudController;
+use App\Models\Importacao;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class InserirTerceirizadoEmMassaJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $dado;
+    protected $dados_importacao;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(array $dado, Importacao $dados_importacao)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $executa = new ImportacaoCrudController();
+        $executa->executaInsercaoMassaTerceirizado($this->dado,$this->dados_importacao);
+    }
+}
