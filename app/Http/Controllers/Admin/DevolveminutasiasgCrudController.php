e04d3ce53587c5e29f500acdf4904ecbf226de55
mvascs <mvascs@mvascss-MacBook-Pro.local>
2021-04-22 15:16:32 -0300

    ajustes finais para o crud

--- app/Http/Controllers/Admin/DevolveminutasiasgCrudController.php
+++ app/Http/Controllers/Admin/DevolveminutasiasgCrudController.php
@@ -1,7 +1,6 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
-
+use App\Models\Devolveminutasiasg;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 
 // inserido
@@ -38,26 +37,61 @@ class DevolveminutasiasgCrudController extends CrudController
 
         (backpack_user()->can('devolveminuta_editar')) ? $this->crud->allowAccess('update') : null;
 
+        // cláusulas para possibilitar buscas
+        $this->crud->addClause('select', 'devolve_minuta_siasg.*', 'devolve_minuta_siasg.created_at as criado_em', 'minutaempenhos.descricao as descricao_minuta_empenho');
+        $this->crud->addClause('join', 'minutaempenhos', 'devolve_minuta_siasg.minutaempenho_id', '=', 'minutaempenhos.id');
+
+        // buscar a descrição da minuta
+        if( \Route::current()->parameter('devolveminutasiasg') ){
+            $idDevolveMinutaSiasg =  \Route::current()->parameter('devolveminutasiasg');
+            $descricao_minuta_empenho = Devolveminutasiasg::where('devolve_minuta_siasg.id','=',$idDevolveMinutaSiasg)
+                ->join('minutaempenhos', 'devolve_minuta_siasg.minutaempenho_id', '=', 'minutaempenhos.id')
+                ->select('minutaempenhos.descricao')
+                ->first()->descricao;
+        } else { $descricao_minuta_empenho = null; }
+
         $this->crud->addColumns($this->colunas());
-        // $campos = $this->Campos($tiposDeEncargo);
-        // $this->crud->addFields($campos);
-
-
+        $campos = $this->Campos($descricao_minuta_empenho);
+        $this->crud->addFields($campos);
 
         /*
         |--------------------------------------------------------------------------
         | CrudPanel Configuration
         |--------------------------------------------------------------------------
         */
-
-        // TODO: remove setFromDb() and manually define Fields and Columns
-        // $this->crud->setFromDb();
-
         // add asterisk for fields that are required in DevolveminutasiasgRequest
         $this->crud->setRequiredFields(StoreRequest::class, 'create');
         $this->crud->setRequiredFields(UpdateRequest::class, 'edit');
-    }
 
+        // filtros na listagem
+        $this->adicionarFiltros();
+    }
+    public function adicionarFiltros()
+    {
+        $this->adicionarFiltroSituacao();
+    }
+    public function adicionarFiltroSituacao()
+    {
+        $arraySituacao = array(
+            'Erro' => 'Erro',
+            'Pendente' => 'Pendente',
+            'Sucesso' => 'Sucesso',
+
+        );
+        $campo = [
+            'name' => 'situacao',
+            'type' => 'select2_multiple',
+            'label' => 'Situação'
+        ];
+        $this->crud->addFilter(
+            $campo,
+            $arraySituacao,
+            function ($value) {
+                $this->crud->addClause('whereIn'
+                    , 'devolve_minuta_siasg.situacao', json_decode($value));
+            }
+        );
+    }
 
     private function colunas(): array
     {
@@ -67,11 +101,11 @@ class DevolveminutasiasgCrudController extends CrudController
                 'label' => 'Minuta empenho', // Table column heading
                 'type' => 'model_function',
                 'function_name' => 'getMinutaEmpenho', // the method in your Model
-                // 'orderable' => true,
-                // 'visibleInTable' => true, // no point, since it's a large text
+                'orderable' => true,
+                'visibleInTable' => true, // no point, since it's a large text
                 // 'visibleInModal' => true, // would make the modal too big
                 // 'visibleInExport' => true, // not important enough
-                // 'visibleInShow' => true, // sure, why not
+                'visibleInShow' => true, // sure, why not
                 // 'searchLogic' => function (Builder $query, $column, $searchTerm) {
                 //     $query->orWhere('codigoitens.descricao', 'ilike', "%$searchTerm%");
                 // },
@@ -82,34 +116,26 @@ class DevolveminutasiasgCrudController extends CrudController
                 'label' => 'Situação', // Table column heading
                 // 'type' => 'model_function',
                 // 'function_name' => 'formatPercentual', // the method in your Model
-                // 'orderable' => true,
+                'orderable' => true,
                 // 'visibleInTable' => true, // no point, since it's a large text
                 // 'visibleInModal' => true, // would make the modal too big
                 // 'visibleInExport' => true, // not important enough
-                // 'visibleInShow' => true, // sure, why not
+                'visibleInShow' => true, // sure, why not
+                // 'searchLogic' => function (Builder $query, $column, $searchTerm) {
+                //     $query->orWhere('devolve_minuta_siasg.situacao', 'ilike', "%$searchTerm%");
+                // },
             ],
             [
-                'name' => 'created_at',
+                'name' => 'criado_em',
                 'label' => 'Criado em', // Table column heading
                 // 'type' => 'model_function',
                 // 'function_name' => 'formatPercentual', // the method in your Model
-                // 'orderable' => true,
+                'orderable' => true,
                 // 'visibleInTable' => true, // no point, since it's a large text
                 // 'visibleInModal' => true, // would make the modal too big
                 // 'visibleInExport' => true, // not important enough
-                // 'visibleInShow' => true, // sure, why not
+                'visibleInShow' => false, // sure, why not
             ],
-            // [
-            //     'name' => 'updated_at',
-            //     'label' => 'Atualizado em', // Table column heading
-            //     // 'type' => 'model_function',
-            //     // 'function_name' => 'formatPercentual', // the method in your Model
-            //     // 'orderable' => true,
-            //     // 'visibleInTable' => true, // no point, since it's a large text
-            //     // 'visibleInModal' => true, // would make the modal too big
-            //     // 'visibleInExport' => true, // not important enough
-            //     // 'visibleInShow' => true, // sure, why not
-            // ],
             [
                 'name' => 'alteracao',
                 'label' => 'Alteração?', // Table column heading
@@ -118,7 +144,7 @@ class DevolveminutasiasgCrudController extends CrudController
                 'visibleInTable' => true, // no point, since it's a large text
                 'visibleInModal' => true, // would make the modal too big
                 'visibleInExport' => true, // not important enough
-                'visibleInShow' => true, // sure, why not
+                'visibleInShow' => false, // sure, why not
                 // optionally override the Yes/No texts
                 'options' => [false => 'Não', true => 'Sim']
 
@@ -128,64 +154,63 @@ class DevolveminutasiasgCrudController extends CrudController
                 'label' => 'Minuta Empenho Remessa', // Table column heading
                 'type' => 'model_function',
                 'function_name' => 'getMinutaEmpenhoRemessa', // the method in your Model
-                // 'orderable' => true,
+                'orderable' => true,
                 // 'visibleInTable' => true, // no point, since it's a large text
                 // 'visibleInModal' => true, // would make the modal too big
                 // 'visibleInExport' => true, // not important enough
-                // 'visibleInShow' => true, // sure, why not
+                'visibleInShow' => false, // sure, why not
+            ],
+            [
+                'name' => 'minutaempenho_id',
+                'label' => 'Minuta Empenho', // Table column heading
+                'type' => 'text',
+                'visibleInTable' => false, // no point, since it's a large text
+                'visibleInShow' => false, // sure, why not
+            ],
+            [
+                'name' => 'minutaempenhos_remessa_id',
+                'label' => 'Minuta Empenho', // Table column heading
+                'type' => 'text',
+                'visibleInTable' => false, // no point, since it's a large text
+                'visibleInShow' => false, // sure, why not
             ],
         ];
     }
 
-    public function Campos($tiposDeEncargo)
+    public function Campos($descricao_minuta_empenho)
     {
+        $arraySituacao = array(
+            'Erro' => 'Erro',
+            'Pendente' => 'Pendente',
+            'Sucesso' => 'Sucesso',
+        );
         $campos = [
-            [
-                // select_from_array
-                'name' => 'tipo_id',
-                'label' => "Tipo",
-                // 'type' => 'text',
-                'type' => 'select2_from_array',
-                'options' => $tiposDeEncargo,
-            ],
-
-            [
-                // Number
-                'name' => 'percentual',
-                'label' => 'Percentual',
-                'type' => 'money',
+            [   //
+                'name' => 'descricao_minuta_empenho',
+                'label' => 'Desc minuta empenho',
+                'type' => 'text',
                 // optionals
                 'attributes' => [
-                    'id' => 'percentual',
+                    'readonly' => 'readonly',
+                    'style' => 'pointer-events: none;touch-action: none;'
                 ], // allow decimals
-                // 'prefix' => "R$",
-                // 'tab' => 'Vigência / Valores',
-                // 'suffix' => ".00",
+                'default' => $descricao_minuta_empenho,
             ],
-
-
-            // [   // Number
-            //     'name' => 'percentual',
-            //     'label' => 'Percentual',
-            //     'type' => 'money',
-            //     // optionals
-            //     'suffix' => " %",
-            //     'default' => 0,
-            //     // 'suffix' => ".00",
-            //     // optionals
-            //     'attributes' => [
-            //         "step" => "any",
-            //         "min" => '1',
-            //     ], // allow decimals
-
-            // ],
+            [ // select_from_array
+                'name' => 'situacao',
+                'label' => "Situação",
+                'type' => 'select2_from_array',
+                'options' => $arraySituacao,
+                // 'options' => config([
+                //     'Erro' => 'Erro',
+                //     'Pendente' => 'Pendente',
+                //     'Sucesso' => 'Sucesso',
+                // ]),
+                'allows_null' => false,
+            ],
         ];
-
         return $campos;
     }
-
-
-
     public function store(StoreRequest $request)
     {
         // your additional operations before save here
@@ -194,7 +219,6 @@ class DevolveminutasiasgCrudController extends CrudController
         // use $this->data['entry'] or $this->crud->entry
         return $redirect_location;
     }
-
     public function update(UpdateRequest $request)
     {
         // your additional operations before save here
