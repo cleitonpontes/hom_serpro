7f53aef1e95fe180c55c2f31c884cefb12616091
Alexssander Saraiva <alex.saraiva.m@gmail.com>
2021-05-14 16:29:49 -0300

    removendo metadados do git

--- app/Http/Middleware/APIAntecipaGov.php
+++ app/Http/Middleware/APIAntecipaGov.php
@@ -14,21 +14,9 @@ class APIAntecipaGov
      * @return mixed
      */
     public function handle($request, Closure $next)
-<<<<<<< HEAD
-<<<<<<< HEAD
     {
 
         if (!backpack_user()->hasPermissionTo('usuario_consulta_api')) {
-=======
-    {   
-        
-        if (!backpack_user()->hasPermissionTo('usuario_inserir')) {
->>>>>>> 109684bbe19df8942143e44f3c5370dddb0749ce
-=======
-    {
-
-        if (!backpack_user()->hasPermissionTo('usuario_consulta_api')) {
->>>>>>> 5c37ddcffb3598dec47872a2f47743d3c54f4835
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
